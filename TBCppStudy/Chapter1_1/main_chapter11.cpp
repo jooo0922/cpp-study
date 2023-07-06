c86db4adbd56b0037417ce61ee59e60d7c49e1fd
jooo0922 <jooo0922@gmail.com>
2023-07-06 23:23:56 +0900

    feat: 변수, 표현식, 선언문, namespace 등 구현

--- TBCppStudy/Chapter1_1/main_chapter11.cpp
+++ TBCppStudy/Chapter1_1/main_chapter11.cpp
@@ -1,8 +1,29 @@
+#include <iostream> // iostream > "input output stream" > 입력과 출력을 구현한 코드를 현재 cpp 파일 안에 포함시킨다는 뜻!
+// #include 처럼 앞에 #이 붙는 것들을 'Preprocessor directive(전처리기)' 라고 함!
+
 // 운영체제가 프로그램을 시작할 때, "main" 이라는 이름의 함수를 가장 먼저 찾게 됨.
 // 만약, main 이라는 이름의 함수가 없다면, 운영체제는 아무 것도 할 수 없어서 프로그램이 종료됨. > main 이라는 이름을 갖는 함수가 반드시 필요한 이유!
-int main()
+// 괄호 안에 void > 입력 타입(아무런 입력이 없으므로 void), 왼쪽 int 는 출력 타입
+int main(void)
 {
+	// 정수형 변수를 선언
+	// int 라는 정수형 데이터를 담을 수 있는 메모리 공간을 할당받는 것
+	int x = 2; 
+	x = 5; // x의 값은 바뀔수도 있다!a
+	int y = x + 3; // 2, 3, 5 이런 값들은 'literal(리터럴)' 이라고 함.
+
+	// 아래 1 + 1 과 같이 문장의 구성요소가 되는 식을 Expression(표현식) 이라고 함.
+	//(1 + 1) * (3 + 4);
+
+	// std 란, cout 이라는 기능의 함수가 정의된 namespace 를 의미 > 비슷한 기능의 함수들을 묶어놓거나, 이름이 겹치는 함수들을 나눠놓기 위해서 사용하는 것!
+	// :: 는 "namespace 안에 들어있는 함수"에 접근할 때 사용
+	// << Output Operator 연산자의 일종!
+	// 특히, std 라고 되어있는 namespace 는, standard library 의 일부 > Visual Studio 를 설치하면 자동으로 딸려서 설치되는 라이브러리들!
+	std::cout << y << std::endl;
+
 	// '프로그래밍'이란 운영체제에게 특정 작업을 해달라고 요청하는 행위!
+	// main 함수의 반환값을 돌려주는 대상은 '운영체제'
+	// 아래 return 문과 같이 기능을 수행하는 문장을 Statement(명령문) 라고 함.
 	return 0;
 }
 
