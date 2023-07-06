2c475fdd3ed68da95b91345922254885769e7c90
jooo0922 <jooo0922@gmail.com>
2023-07-07 00:25:32 +0900

    feat: 실습예제 구현

--- TBCppStudy/Chapter1_3/Chapter1_3.cpp
+++ TBCppStudy/Chapter1_3/Chapter1_3.cpp
@@ -8,10 +8,10 @@ int main()
     //int x;
     //x = 123; // = 은 Assignment Operator (할당 연산자) > '같다' 는 의미가 아님. x 라는 변수(이름이 가리키고 있는 메모리 공간)에 123을 대입해준다!
 
-    int x = 123; // initialization (초기화) 여기서 = 연산자는 x라는 이름의 메모리 공간이 마련될 때 바로 123을 복사해서 넣어버림 > 초기화! (할당과는 다르다!)
-    int y(123); // 이런 식으로도 초기화가 가능함
+    //int x = 123; // initialization (초기화) 여기서 = 연산자는 x라는 이름의 메모리 공간이 마련될 때 바로 123을 복사해서 넣어버림 > 초기화! (할당과는 다르다!)
+    //int y(123); // 이런 식으로도 초기화가 가능함
 
-    x = 5; // Assignment(할당) 여기서 = 연산자는 x라는 이름을 갖는 메모리 공간에 5라는 값을 '할당' 하는 행위!
+    //x = 5; // Assignment(할당) 여기서 = 연산자는 x라는 이름을 갖는 메모리 공간에 5라는 값을 '할당' 하는 행위!
 
     /*
         위 선언문에서 왼쪽에 있는 x 는 Left Value, > 주소를 갖고 있는 것
@@ -25,8 +25,26 @@ int main()
         그래서 x 역시 '값'으로 바꿔치기 됨!
     */
 
-    std::cout << x << std::endl;
-    std::cout << &x << std::endl; // x 는 결국 '어떤 메모리 공간의 이름' 임을 알 수 있음. > & ampersand 를 붙여서 콘솔 출력 시, 16진수로 표현된 실제 메모리 주소가 출력됨!
+    //std::cout << x << std::endl;
+    //std::cout << &x << std::endl; // x 는 결국 '어떤 메모리 공간의 이름' 임을 알 수 있음. > & ampersand 를 붙여서 콘솔 출력 시, 16진수로 표현된 실제 메모리 주소가 출력됨!
+
+    //return 0;
+
+    // 연습 예제
+    int x = 1;
+    x = x + 2;
+    std::cout << x << std::endl; // 3
+
+    int y = x;
+    std::cout << y << std::endl; // 3
+
+    // (x + y) 는 r-value, 즉 메모리 주소가 없는 '값'
+    std::cout << x + y << std::endl; // 6
+
+    std::cout << x << std::endl; // 3
+
+    int z;
+    //std::cout << z << std::endl; // 에러 > 메모리 공간을 준비했지만, 값이 할당되지 않음
 
     return 0;
 }
