48b020bfe15611794f0d3cf2a2b7b7d914c4a049
jooo0922 <jooo0922@gmail.com>
2023-07-07 00:01:53 +0900

    feat: & 앰퍼샌드로 변수의 실제 메모리 주소 출력

--- TBCppStudy/Chapter1_3/Chapter1_3.cpp
+++ TBCppStudy/Chapter1_3/Chapter1_3.cpp
@@ -9,6 +9,7 @@ int main()
     x = 123; // = 은 Assignment Operator (할당 연산자) > '같다' 는 의미가 아님. x 라는 변수(이름이 가리키고 있는 메모리 공간)에 123을 대입해준다!
 
     std::cout << x << std::endl;
+    std::cout << &x << std::endl; // x 는 결국 '어떤 메모리 공간의 이름' 임을 알 수 있음. > & ampersand 를 붙여서 콘솔 출력 시, 16진수로 표현된 실제 메모리 주소가 출력됨!
 
     return 0;
 }
