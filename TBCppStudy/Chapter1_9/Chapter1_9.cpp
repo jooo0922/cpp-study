d918c88581ea28ba9cbc55e9cd024a5b1ce15143
jooo0922 <jooo0922@gmail.com>
2023-07-09 23:50:12 +0900

    feat: 코드 포맷팅 연습

--- TBCppStudy/Chapter1_9/Chapter1_9.cpp
+++ TBCppStudy/Chapter1_9/Chapter1_9.cpp
@@ -1,20 +1,35 @@
-﻿// Chapter1_9.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using namespace std;
+
+// 피연산자 간 공백을 한칸씩 띄워주기
+// 함수 블록 중괄호 작성 시, 중괄호를 열고 닫을 때마다 개행을 해주기
+int add(int x, int y) 
+{ 
+    return x + y; // indenting 도 반드시 탭 1칸(스페이스 4칸) 만큼을 띄워줘야
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // 줄이 길어져서 개행을 해야 할 경우,
+    // 아래와 같이 << 연산자를 남겨놓고 개행을 하면,
+    // 다른 개발자가 볼때, "아, 여기 다음 줄에 뭔가를 또 출력하는 게 이어서 나오겠구나" 라고 무의식적으로 확인할 수 있음!
+    cout << "Hello, World!" << "abcdef" << 
+        "Hello Home" << endl;
+
+    // 하드코딩 : 프로그래머가 변수의 값을 코드에서 직접 대입하는 방식
+    // 소프트코딩 : 런타임에 사용자 입력, 외부 파일, 네트워트 통신 등으로 데이터를 가져오는 방식
+    // 당연히 소프트코딩 방식이 더 좋겠지?
+    // 또, 변수에 값을 할당하는 코드를 탭 키로 아래와 같이 줄맞춤 해줄 수 있음. 
+    int my_v            = 1;
+    int x               = 4;
+    int num_apples      = 123;
+
+    // this is important > 주석이 길어질 경우 코드 오른쪽보다는 코드 위쪽에 달 것!
+    int x = 1 + 2; 
+
+    // this is important, too
+    int y = 3 + 4; 
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
