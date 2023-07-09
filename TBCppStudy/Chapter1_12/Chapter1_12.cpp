3e945fd291d3ad773d76072fe1132f0f13baea41
jooo0922 <jooo0922@gmail.com>
2023-07-10 01:58:02 +0900

    feat: 헤더가드 제거 시 발생 에러 재현

---
+++ TBCppStudy/Chapter1_12/Chapter1_12.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "my_work.h"
+#include "add.h" // 프로그래밍을 하다 보면, my_work.h 에서 include 해야 할 헤더파일을 여기서 하게 되는 경우가 발생!
+
+using namespace std;
+
+int main()
+{
+	doSomething();
+
+	return 0;
+}
