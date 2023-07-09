cf2eca18ae1397f0592478241a05e6086a0ea891
jooo0922 <jooo0922@gmail.com>
2023-07-10 01:41:46 +0900

    refactor: 헤더파일 하위폴더 변경

--- TBCppStudy/Chapter1_11/Chapter1_11.cpp
+++ TBCppStudy/Chapter1_11/Chapter1_11.cpp
@@ -1,5 +1,7 @@
-#include <iostream>
-#include "add.h"
+#include <iostream> // 얘는 좀 특별한 템플릿이라 꺽쇠로 가져옴
+#include "MyHeaders/add.h" // 현재 빌드 위치에서 하위 폴더로 옮겼을 경우, "폴더이름/헤더파일명" 이런 식으로 작성해줘야 함.
+// 또, 우측의 Solution Exploer > Chapter1_11 > Header Files 에서 해당하는 헤더파일을 Remove 하고, 
+// 파일 탐색기로부터 옮긴 헤더파일 위치를 찾은 다음, Header Files 에 드래그앤드랍 해줘야 함.
 
 using namespace std;
 
