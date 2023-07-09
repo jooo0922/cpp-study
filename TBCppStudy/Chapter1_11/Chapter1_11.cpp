9701d5a0ccfa29cd6d3d5661f8e646a783b28e3b
jooo0922 <jooo0922@gmail.com>
2023-07-10 01:33:25 +0900

    refactor: 전방선언을 헤더파일로 이동

--- TBCppStudy/Chapter1_11/Chapter1_11.cpp
+++ TBCppStudy/Chapter1_11/Chapter1_11.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include "add.h"
 
 using namespace std;
 
-int add(int a, int b);
+// 전방선언으로 함수를 복붙하는 것도 번거로울 수 있다! 
+// -> 헤더파일에 선언해놓으면, 다른 파일에서도 중복해서 전방선언을 할 필요가 없다!
+//int add(int a, int b);
 
 int main()
 {
     cout << add(1, 2) << endl;
+
+    return 0;
 }
---
+++ TBCppStudy/Chapter1_11/add.h
@@ -0,0 +1,3 @@
+#pragma once // 헤더가드 (전처리기의 일종)
+
+int add(int a, int b);
