3e64e89fdc94e4e306d6f896e4ea5152341f65cb
jooo0922 <jooo0922@gmail.com>
2023-07-23 19:41:52 +0900

    feat: 부동소수점 표현 시 주의점 연습

--- TBCppStudy/Chapter2_5/Chapter2_5.cpp
+++ TBCppStudy/Chapter2_5/Chapter2_5.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include <iomanip>
+#include <iomanip> // io manipulator 즉, 입출력 조작 관련 라이브러리 > 숫자를 몇째자리 수까지 출력할 것인지 옵션 조정 가능
 #include <limits> // 컴파일러에게 물어보는 함수가 담긴 라이브러리가 <limits>
+#include <cmath> // std::isnan() 함수를 사용하기 위해 포함한 라이브러리 (현재 숫자가 nan 인지 boolean 값으로 반환)
 
 int main()
 {
@@ -31,6 +32,36 @@ int main()
     cout << 31.4e-2 << endl; // 31.4 * 10^-2
     cout << 31.4e1 << endl; // 31.4 * 10^1
     cout << 31.4e2 << endl; // 31.4 * 10*2
+
+    cout << std::setprecision(16); // setprecision(몇째자리) 로 앞으로의 출력에 대해 몇째자리 수까지 출력할 지 옵션 설정 가능
+    cout << 1.0 / 3.0 << endl; // 그냥 출력하면 0.333333 까지만 출력됨. 이거보다 더 길게 0.3333333333333333.... 쯕 나오게 하려면 iomanip 라이브러리를 사용해야 함!
+
+    float ff(123456789.0f); // 10 significant digits (유효숫자 10개)
+
+    cout << std::setprecision(9); // 9번째 자릿수까지 출력하도록 설정
+    cout << ff << endl; // 123456792 이런 식으로 나옴. > 부동소수점이 이진수로 저장되어 있어 우리가 생각하는 것과 다르게 나옴
+
+    double dd(0.1);
+    cout << dd << endl; // 0.1
+    cout << std::setprecision(17); // 17번째 자릿수까지 출력하도록 설정
+    cout << dd << endl; // 0.10000...1 (마지막에 1이 하나 더 붙음 > 이진수로 부동소수점 표현할 수 있는 0.1에 가장 가까운 수가 이거라고 생각하면 됨.)
+
+    // 상식적으로 d1 과 d2 는 출력 시 동일하게 1이 나와야 할 것 같음.
+    double d1(1.0);
+    double d2(0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1);
+    cout << setprecision(17); // 17번째 자릿수까지 출력하도록 설정
+    cout << d1 << endl; // 1
+    cout << d2 << endl; // 0.9999...89 (위에 dd 를 17번째 자릿수까지 출력했을 때 0.1000...1 이 나왔던 것처럼, d2 에서 0.1 들의 오차가 누적되어 17번째 자릿수까지 출력하면 실제 1과 다른 값으로 출력됨.)
+
+    double zero = 0.0;
+    double posinf = 5.0 / zero; // 5.0 을 0.0 으로 나누면? positive infinity (양의 무한대)
+    double neginf = -5.0 / zero; // 5.0 을 0.0 으로 나누면? negative infinity (양의 무한대)
+    double nan = zero / zero; // Not a Number
+
+    cout << posinf << " " << std::isinf(posinf) << endl; // inf
+    cout << neginf << " " << std::isinf(neginf) << endl; // -inf
+    cout << nan << " " << std::isnan(nan) << endl; // -nan(ind) > 여기서 ind 는 indeterminate, 즉, '결정할 수 없다' 정도의 뜻을 가짐.
+    cout << 1.0 << " " << std::isnan(1.0) << endl; // 1.0 은 nan 이 아니므로 0(false) 가 출력
 
     return 0;
 }
