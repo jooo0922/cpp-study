7bd4e8702a6787718715dddebe401e819e99b2f0
jooo0922 <jooo0922@gmail.com>
2023-07-29 15:38:14 +0900

    chore: Chapter3_7 프로젝트 생성

---
+++ TBCppStudy/Chapter3_7/Chapter3_7.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    /*
+    
+        0
+        1
+        2
+        3
+        4
+        5
+        6
+        7
+        8
+        9
+        10 = 10^1 + 0
+        11 = 10^1 + 1
+        12
+        13
+        ...
+
+    */
+
+    return 0;
+}
