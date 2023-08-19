5772b9f60beb73fdb1f3c1fe555dee7388ef2dcc
jooo0922 <jooo0922@gmail.com>
2023-08-19 17:34:53 +0900

    chore: Chapter5_03 프로젝트 생성

---
+++ TBCppStudy/Chapter5_03/Chapter5_03.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+enum class Colors
+{
+    BLACK,
+    WHITE,
+    RED,
+    GREEN,
+    BLUE
+};
+
+void printColorName(Colors color)
+{
+
+}
+
+int main()
+{
+    return 0;
+}
