b1258f29af579ad75612f08cf8002e9065e6cce0
jooo0922 <jooo0922@gmail.com>
2023-08-22 00:43:38 +0900

    chore: Chapter5_10 프로젝트 생성

---
+++ TBCppStudy/Chapter5_10/Chapter5_10.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int getInt()
+{
+    cout << "Enter an Integer number : ";
+    int x;
+    cin >> x;
+
+    return x;
+}
+
+char getOperator()
+{
+    cout << "Enter an operator (+, -) : "; // TODO: more operators *, / etc.
+    char op;
+    cin >> op;
+
+    return op;
+}
+
+void printResult(int x, char op, int y)
+{
+    if (op == '+') cout << x + y << endl;
+    else if (op == '-') cout << x - y << endl;
+    else
+    {
+        cout << "Invalid operator" << endl;
+    }
+}
+
+int main()
+{
+    int x = getInt();
+    char op = getOperator();
+    int y = getInt();
+
+    printResult(x, op, y);
+
+    return 0;
+}
