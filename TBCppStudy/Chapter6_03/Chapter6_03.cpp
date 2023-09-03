03f9f647f02da3f12392939eb2a8cac9e6c14ea0
jooo0922 <jooo0922@gmail.com>
2023-09-03 15:17:56 +0900

    feat: 배열과 반복문으로 최저점, 최고점 계산

--- TBCppStudy/Chapter6_03/Chapter6_03.cpp
+++ TBCppStudy/Chapter6_03/Chapter6_03.cpp
@@ -21,11 +21,17 @@ int main()
 
     const int num_students = sizeof(scores) / sizeof(int); // 이런 식으로, 초기화된 배열로부터 거꾸로 배열의 크기를 역계산할 수 있음
 
+    int max_score = 0; // 최고점을 저장할 변수
+    int min_score = 100; // 최저점을 저장할 변수 (만점(100)으로 초기화함)
     int total_score = 0;
 
     for (int i = 0; i < num_students; ++i)
     {
         total_score += scores[i];
+
+        //max_score = (max_score < scores[i]) ? scores[i] : max_score; // 반복문을 돌면서 현재까지의 최고점과 각 배열 요소를 비교하며 최고점 갱신
+        if (max_score < scores[i]) max_score = scores[i]; // if 문으로도 표현 가능
+        if (min_score > scores[i]) min_score = scores[i];
     }
 
     double avg_score = static_cast<double>(total_score) / num_students;
@@ -39,6 +45,9 @@ int main()
         결과값이 정수로 딱 떨어지지 않을 경우,
         소수 부분이 유실되어 정확하지 않은 값으로 계산될 수 있음!
     */
+
+    cout << max_score << endl; // 최고점 출력
+    cout << min_score << endl; // 최저점 출력
 
     return 0;
 }
