888d9d8dead8ec5028fe30688037fe5aace6f335
jooo0922 <jooo0922@gmail.com>
2023-09-03 15:21:59 +0900

    chore: Chapter6_04 프로젝트 생성

---
+++ TBCppStudy/Chapter6_04/Chapter6_04.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*                  value       index
+        3 5 2 1 4
+        1 5 2 3 4
+        1 2 5 3 4
+        1 2 3 5 4
+        1 2 3 4 5
+    */
+    
+    return 0;
+}
