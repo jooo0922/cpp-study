59807d193230a7ce6e838e584ec362f9142b53f4
jooo0922 <jooo0922@gmail.com>
2023-09-03 15:57:46 +0900

    feat: 배열과 반복문으로 선택 정렬 구현

--- TBCppStudy/Chapter6_04/Chapter6_04.cpp
+++ TBCppStudy/Chapter6_04/Chapter6_04.cpp
@@ -2,6 +2,16 @@
 
 using namespace std;
 
+// 배열을 순회하며 출력하는 함수 추출
+void printArray(const int array[], const int length) // 매개변수를 함수 내에서 변경하지 않을 것이므로, const 를 붙여서 데이터를 불변으로 만듦. > 실수를 최대한 줄이기 위함.
+{
+    for (int index = 0; index < length; ++index)
+    {
+        cout << array[index] << " ";
+    }
+    cout << endl;
+}
+
 int main()
 {
     /*                  value       index
@@ -17,11 +27,47 @@ int main()
 
     int array[length] = { 3, 5, 2, 1, 4 };
 
-    for (int index = 0; index < length; ++index)
+    printArray(array, length);
+
+    // 위 알고리즘에서 사용된 swap (배열 요소 간 위치 변경)은 어떻게 구현하나?
+    // int temp = array[0]; // 일반적으로, 교체할 배열 요소의 값을 임시로 보관해놓는 변수 temp 에 값을 복사해 놓고 사용함.
+    // array[0] = array[1];
+    // array[1] = temp;
+    // printArray(array, length); // swap 된 배열 확인
+    // 위 방식과 동일한 기능을 하는 함수가 std::swap() 으로 라이브러리에 구현되어 있음!
+
+    // 위 알고리즘 대로라면, 
+    // 하나의 배열을 각 배열 요소를 출발점으로 하여 순회하고, 
+    // 또 각 배열 요소 출발점부터 마지막 배열 요소까지 순회를 해야 하니, 
+    // 이중 for loop 를 사용해야 한다는 걸 눈치챘을 것임!
+    for (int startIndex = 0; startIndex < length - 1; ++startIndex) // 위 알고리즘에서 마지막 번째 요소는 출발점으로 두고 순회하지 않았으므로, length - 1(출발점으로 두고 순회할 배열 요소의 개수는 마지막 요소는 제외!) 만큼만 반복문을 돌려줘야겠지!
     {
-        cout << array[index] << " ";
+        int smallestIndex = startIndex; // 위 알고리즘에서 출발점 요소 이후의 요소들과 비교하면서 가장 작은 요소의 index 값을 저장해뒀었지? 그걸 저장해둘 변수!
+
+        // 안쪽 반복 루프에서는 각 배열 요소를 반복 순회의 출발점으로 지정하기 때문에,
+        // 안쪽 반복문의 초기 인덱스 값은 각 배열 요소의 인덱스 + 1 로 초기화함.
+        for (int currentIndex = startIndex + 1; currentIndex < length; ++currentIndex)
+        {
+            if (array[smallestIndex] > array[currentIndex])
+            {
+                // 각 반복 루프의 출발점 이후의 요소들과 비교하면서 더 작은 요소의 index 를 계속 갱신함.
+                // 참고로, 우리는 index 값만 알고 있으면 index 에 대응되는 value 는 array[index] 로 접근 가능하므로,
+                // index 에 대응되는 value 는 별도로 저장하지 않고 있음.
+                smallestIndex = currentIndex;
+            }
+        }
+
+        // 안쪽 반복 루프를 순회하고 나면, 출발점부터 각 요소들끼리 value 의 크기를 비교해서
+        // 가장 작은 value 에 해당하는 index 가 smallestIndex 에 저장되어 있겠지?
+        // 가장 작은 값을 찾았다면, 위에 알고리즘대로 출발점 요소와 가장 작은 value 에 해당하는 요소를 swapping 해줬었지?
+        // std::swap() 으로도 가능하다고 했지?
+        // std::swap(array[smallestIndex], array[startIndex]); 요렇게 사용해주는 것과 동일함!
+        int temp = array[smallestIndex];
+        array[smallestIndex] = array[startIndex];
+        array[startIndex] = temp;
+
+        printArray(array, length); // 이렇게 출발점 요소가 바뀔 때마다(즉, swap 이 일어날 때마다) 현재 변경된 array 상태를 출력해보면, 위 알고리즘에 정렬 과정과 똑같이 출력되는 걸 볼 수 있음!
     }
-    cout << endl;
 
     return 0;
 }
