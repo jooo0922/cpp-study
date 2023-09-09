6f530c12a8cf19ab9c25b9458b7c141e6e0009cc
jooo0922 <jooo0922@gmail.com>
2023-09-09 22:18:00 +0900

    feat: 배열 매개변수로 전달 및 포인터 연산 테스트

--- TBCppStudy/Chapter6_08/Chapter6_08.cpp
+++ TBCppStudy/Chapter6_08/Chapter6_08.cpp
@@ -2,9 +2,18 @@
 
 using namespace std;
 
+ //void printArray(int *array) // 아래 함수는 이렇게 선언하는 것과 동일함.
 void printArray(int array[])
 {
-    cout << sizeof(array) << endl;
+    // 매개변수를 마치 배열처럼 array[] 이렇게 선언해놔도,
+    // 내부적으로는 이것을 포인터 배열로 처리하고 있음.
+    // 그래서, main() 함수에서처럼 array 배열에 저장된 모든 요소의 포인터 크기를 합친 20 bytes 가 나오는 게 아니라,
+    // 매개변수가 하나의 포인터 변수로 선언된 것이기 때문에,
+    // 모든 포인터 변수에 저장된 주소값의 동일한 크기인 4 bytes 가 출력됨
+    cout << sizeof(array) << endl; // 4 bytes
+    cout << *array << endl; // 9
+
+    *array = 100; // 함수의 마지막 부분에서 배열의 첫 번째 요소에 대한 de-referencing 의 값을 100으로 변경한다면?
 }
 
 int main()
@@ -56,6 +65,21 @@ int main()
     // array 에 담긴 전체 요소의 메모리 주소값의 사이즈가 합쳐져서 반환됨.
     cout << sizeof(array) << endl; // 4 bytes * 5 = 20 bytes
     cout << sizeof(ptr) << endl; // 4 bytes (기본적으로 모든 타입의 포인터 변수에 저장된 메모리 주소값은 x86 아키텍쳐 기준 4 bytes 로 동일하다고 했었지?)
+
+    //printArray(array);
+
+    // printArray() 함수의 마지막 부분에서 de-referencing 된 배열 첫 번째 요소의 값을 100으로 변경했었지?
+    // 이 값이 어떻게 바뀌었는지 직접 확인해보자
+    // 외부에서 선언된 배열 변수의 de-referencing 된 값을 함수 내부에서 변경할 수 있음!
+    // 이런 식으로, 배열(포인터 변수)를 de-referencing 된 값을 함수 내부에서 변경할 때 포인터 변수를 사용하기도 함!
+    //cout << array[0] << " " << *array << endl; // 100 100
+
+    // 배열의 첫 번째 메모리 주소값에 +1 을 하면 어떻게 될까?
+    // 해당 메모리 공간 '옆'에 있는 메모리 공간의 주소를 얻을 수 있음.
+    // 이 원리를 배열에서 응용한다면, 배열의 첫 번째 요소의 메모리 주소값에 +1 해주면
+    // 배열의 두 번째 요소가 저장된 메모리의 주소값을 얻을 수 있겠지!
+    // 이렇게 포인터 변수의 메모리 주소값에 대해서 어떠한 연산을 해주는 것을 '포인터 연산' 이라고 함.
+    cout << *ptr << " " << *(ptr + 1) << endl; // 9 7 > 배열의 첫 번째 요소와 두 번째 요소의 주소값을 de-referencing 한 것을 출력한 셈!
 
     return 0;
 }
