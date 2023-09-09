a7e21754a46db3796c9bbc2bcde258ee81a9c74e
jooo0922 <jooo0922@gmail.com>
2023-09-09 20:55:10 +0900

    feat: 포인터 변수의 '주소값의 주소값' 비교

--- TBCppStudy/Chapter6_07a/Chapter6_07a.cpp
+++ TBCppStudy/Chapter6_07a/Chapter6_07a.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstddef>
 
 using namespace std;
 
@@ -7,6 +8,28 @@ using namespace std;
 // 널 체크를 먼저 검사하고 작업을 수행하는 패턴으로 많이 사용됨.
 void doSomething(double* ptr)
 {
+    /*
+        doSomething() 안에서는 매개변수에 선언한 포인터 변수의 
+        '주소값의 주소값'을 찍어보자
+
+        main() 에서 출력한 포인터의 '주소값의 주소값' 과 '다르다'!!!!!!
+
+        왜냐하면, 기본적으로 c++ 함수에서는 파라미터로 전달된 변수의 값을 복사해서
+        함수에 선언한 매개변수에 할당하기 때문에, 
+        main() 에 있던 ptr 변수에 담긴 메모리 주소값이
+        doSomething() 에 선언된 매개변수에 '복사'가 되어서 할당됨!
+
+        결과적으로, main() 에 있던 ptr 포인터 변수와 
+        doSomething() 의 매개변수인 ptr 포인터 변수는
+        '서로 다른 메모리 공간'을 할당받은 '서로 다른 변수' 라는 것이다!
+
+        그래서, 이 main() 에 있는 
+        포인터 변수의 '주소값의 주소값' 을 가져와서 직접 참조하기 위해
+        매개변수를 '이중 포인터'로 선언해서 사용하는 기법이 아주 많이 사용됨!
+        자료구조&알고리즘 책에서도 이런 방식으로 구현한 것이 많음!
+    */
+    cout << "address of pointer variable in doSomething() " << &ptr << endl;
+
     if (ptr != nullptr)
     {
         // do something useful
@@ -55,6 +78,17 @@ int main()
     ptr = &d;
 
     doSomething(ptr); // 변수 d의 주소값이 할당된 ptr 포인터 변수 입력 > de-referencing 출력
+
+    // cstddef 라이브러리에서 가져온 타입으로,
+    // nullptr 만 파라미터로 받는 함수일 경우, nullptr 타입 변수를 선언할 때 사용.
+    // 근데 실제로 많이 사용할 일은 없다고 함.
+    nullptr_t nptr;
+
+    // 이제 포인터 변수에 담긴 주소값이 아닌,
+    // 포인터 변수에 할당된 메모리 공간의 주소값,
+    // 즉, 포인터 변수에 할당된 주소값이 저장된 메모리 공간의 주소값,
+    // 즉, '주소값의 주소값' 을 출력해보자!
+    cout << "address of pointer variable in main() " << &ptr << endl;
 
     return 0;
 }
