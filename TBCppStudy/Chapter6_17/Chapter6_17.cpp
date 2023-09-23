6ab1a6ed9ccab2c60b4ca35e1ad8b643b000d5bc
jooo0922 <jooo0922@gmail.com>
2023-09-23 17:19:50 +0900

    feat: for-each 문으로 배열 출력

--- TBCppStudy/Chapter6_17/Chapter6_17.cpp
+++ TBCppStudy/Chapter6_17/Chapter6_17.cpp
@@ -6,5 +6,12 @@ int main()
 {
     const int fibonacci[] = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 };
     
+    // for-each 문으로 배열 순회하기
+    // 배열의 각 요소를 출력하기
+    for (int number : fibonacci)
+        cout << number << " ";
+
+    cout << endl;
+
     return 0;
 }
