14c8958e3d85f145ee9c2f9449cccd0d90231551
jooo0922 <jooo0922@gmail.com>
2023-10-04 20:17:42 +0900

    chore: Chapter7_05 프로젝트 생성

---
+++ TBCppChapter7/Chapter7_05/Chapter7_05.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int getValue(int x)
+{
+    int value = x * 2;
+    return value;
+}
+
+int main()
+{
+    int value = getValue(3);
+
+    return 0;
+}
