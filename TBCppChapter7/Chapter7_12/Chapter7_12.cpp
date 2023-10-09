c8d5e4281433ef98ede25336ab78ab3f76731ad2
jooo0922 <jooo0922@gmail.com>
2023-10-09 14:29:23 +0900

    feat: 재귀호출로 피보나치 수열 출력 함수 구현

--- TBCppChapter7/Chapter7_12/Chapter7_12.cpp
+++ TBCppChapter7/Chapter7_12/Chapter7_12.cpp
@@ -22,10 +22,24 @@ int sumTo(int sumto)
         return sumTo(sumto - 1) + sumto;
 }
 
+// 피보나치 수열을 재귀 호출로 출력하는 함수
+void printFibonacci(unsigned int firstTerm, unsigned int secoundTerm)
+{
+    unsigned int nextTerm = firstTerm + secoundTerm; // 수열의 다음 항을 앞 두 항의 합으로 계산
+    cout << firstTerm << endl; // 앞 두 항에서 첫째 항만 출력
+
+    // 메모리 크기 한계 상, 수열의 항이 100,000 을 넘어가면 재귀호출하지 않도록 함. 
+    if (nextTerm < 100000)
+        printFibonacci(secoundTerm, nextTerm); // 둘째 항과 다음 항을 인자로 넘겨서 재귀호출
+}
+
 int main()
 {
     //countDown(5);
 
-    cout << sumTo(10) << endl;
+    //cout << sumTo(10) << endl;
+
+    printFibonacci(0, 1);
+
     return 0;
 }
