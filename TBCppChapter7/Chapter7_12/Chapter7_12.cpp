4a587c1cc5dfdd5735b0e74f98cfbdc7114708c3
jooo0922 <jooo0922@gmail.com>
2023-10-09 14:16:24 +0900

    feat: 임의의 수보다 작은 수들의 합을 재귀 호출로 구현

--- TBCppChapter7/Chapter7_12/Chapter7_12.cpp
+++ TBCppChapter7/Chapter7_12/Chapter7_12.cpp
@@ -11,8 +11,21 @@ void countDown(int count)
         countDown(count - 1); // 함수가 자기 자신을 호출하는 함수 > 재귀적 함수 호출
 }
 
+// 인자로 전달한 sumto 보다 작은 숫자들의 합을 재귀 호출로 계산
+int sumTo(int sumto)
+{
+    if (sumto <= 0)
+        return 0;
+    else if (sumto <= 1)
+        return 1;
+    else
+        return sumTo(sumto - 1) + sumto;
+}
+
 int main()
 {
-    countDown(5);
+    //countDown(5);
+
+    cout << sumTo(10) << endl;
     return 0;
 }
