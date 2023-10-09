531e22d143ca738984f01e7d63ddfc9caaccb442
jooo0922 <jooo0922@gmail.com>
2023-10-09 12:43:18 +0900

    feat: 함수 포인터로 선언된 매개변수에 기본값 할당

--- TBCppChapter7/Chapter7_09/Chapter7_09.cpp
+++ TBCppChapter7/Chapter7_09/Chapter7_09.cpp
@@ -30,7 +30,8 @@ bool isOdd(const int& number)
 // 배열을 const 참조변수(=입력 매개변수) 로 받아서 짝수 또는 홀수 출력 함수
 // 이때, 짝수 또는 홀수 여부 검사 함수를 함수 포인터 변수 매개변수를 선언하여 받음!
 // -> 함수 포인터를 다른 함수의 매개변수로 선언하여 사용할 수 있구나!
-void printNumbers(const array<int, 10>& my_array, bool(*check_fcn)(const int&))
+// 또한, 함수 포인터 매개변수에는 기본값(default)도 할당 가능!
+void printNumbers(const array<int, 10>& my_array, bool(*check_fcn)(const int&) = isEven)
 {
     for (auto element : my_array)
     {
@@ -66,7 +67,7 @@ int main()
     std::array<int, 10> my_array = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
     // 함수 포인터를 매개변수로 선언했기 때문에, 함수를 직접 인자로 전달 가능
-    printNumbers(my_array, isEven);
+    printNumbers(my_array); // 아무런 인자를 전달하지 않으면, 기본값 함수 포인터인 isEven 이 실행될 것임.
     printNumbers(my_array, isOdd);
 
     return 0;
