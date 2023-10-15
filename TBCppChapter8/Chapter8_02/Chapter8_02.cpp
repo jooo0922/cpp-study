98346703d113cc0f9180e6b306635445be763653
jooo0922 <jooo0922@gmail.com>
2023-10-15 22:19:14 +0900

    chore: Chapter8_02 프로젝트 생성

---
+++ TBCppChapter8/Chapter8_02/Chapter8_02.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Date
+{
+    int m_month;
+    int m_day;
+    int m_year;
+};
+
+int main()
+{
+    return 0;
+}
