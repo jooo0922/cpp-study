3f3a421d8ce552ffeff3f31d5cfc5341aecc8d03
jooo0922 <jooo0922@gmail.com>
2023-10-21 23:11:28 +0900

    feat: this 를 활용한 멤버함수 체이닝

--- TBCppChapter8/Chapter8_07/Chapter8_07.cpp
+++ TBCppChapter8/Chapter8_07/Chapter8_07.cpp
@@ -33,6 +33,30 @@ public:
 	int getID() { return m_id; }
 };
 
+class Calc
+{
+private:
+	int m_value;
+
+public:
+	Calc(int init_value)
+		: m_value(init_value)
+	{}
+
+	// 이 메서드들은 반환타입이 클래스 자신의 '참조변수'로 선언됨. (포인터 변수 아님 주의!)
+	// (참조변수 관련 https://github.com/jooo0922/cpp-study/blob/main/TBCppStudy/Chapter6_14/Chapter6_14.cpp 참고)
+	// 즉, 실제 값(인스턴스)이 저장된 변수를 반환해야 하므로, 
+	// 인스턴스 자신의 주소값이 담긴 this 를 de-referencing 하여 실제 인스턴스 자신을 반환함.
+	// 이처럼, 메서드 마지막에 인스턴스 자신(*this)을 반환하면, 해당 함수를 멤버함수 체이닝에 사용할 수 있음!
+	Calc& add(int value) { m_value += value; return *this; }
+	Calc& sub(int value) { m_value -= value; return *this; }
+	Calc& mult(int value) { m_value *= value; return *this; }
+
+	void print()
+	{
+		cout << m_value << endl;
+	}
+};
 
 int main()
 {
@@ -43,6 +67,11 @@ int main()
 	// 두 인스턴스 s1, s2 를 어떻게 구분할까?
 	// 가장 간단한 방법은 두 인스턴스의 주소를 찍어보는 것
 	cout << &s1 << " " << &s2 << endl;
+
+
+	/* 인스턴스 자신(*this)를 반환하는 메서드를 활용한 멤버함수 연쇄 호출(member function chaining) */
+	Calc cal(10);
+	cal.add(10).sub(1).mult(2).print();
 
     return 0;
 }
