8c31979b165a912e29ea175d136559cb1404c4c5
jooo0922 <jooo0922@gmail.com>
2023-10-21 22:07:39 +0900

    feat: 클래스 멤버를 멤버 초기화 리스트로 초기화

--- TBCppChapter8/Chapter8_04/Chapter8_04.cpp
+++ TBCppChapter8/Chapter8_04/Chapter8_04.cpp
@@ -2,6 +2,15 @@
 
 using namespace std;
 
+class B
+{
+private:
+	int m_b;
+
+public:
+	B(const int& m_b_in) : m_b(m_b_in) {}
+};
+
 class Something
 {
 private:
@@ -9,13 +18,13 @@ private:
 	double m_d;
 	char m_c;
 	int m_arr[5]; // c++ 11 부터는 정적 배열도 멤버 초기화 리스트로 초기화 가능
+	B m_b; // 클래스인 멤버변수도 멤버 초기화 리스트로 초기화 가능
 
 public:
 	// 멤버 초기화 리스트 (member initializer list) 로 생성자 호출 시점에 멤버변수들을 미리 초기화할 수 있음.
 	// 이떄, m_i() 이렇게 초기화하기 보다는, m_i{} 로 uniform initialization 해주는 게 형변환을 막아 엄격한 타입 체크를 가능하게 해줌.
 	// https://github.com/jooo0922/raytracing-study/blob/main/InOneWeekend/InOneWeekend/ray.h 에서 멤버 초기화 리스트를 한 번 사용했었음.
-	Something()
-		: m_i{ 1 }, m_d{ 3.14 }, m_c{ 'a' }, m_arr{ 1, 2, 3, 4, 5 }
+	Something() : m_i{ 1 }, m_d{ 3.14 }, m_c{ 'a' }, m_arr{ 1, 2, 3, 4, 5 }, m_b(m_i - 1)
 	{
 		/*m_i = 1;
 		m_d = 3.14;
