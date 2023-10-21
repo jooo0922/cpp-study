4b927650390005cebd751f3d15879a037987bab9
jooo0922 <jooo0922@gmail.com>
2023-10-21 21:51:11 +0900

    chore: Chapter8_04 프로젝트 생성

---
+++ TBCppChapter8/Chapter8_04/Chapter8_04.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+class Something
+{
+private:
+	int m_i;
+	double m_d;
+	char m_c;
+
+public:
+	Something();
+
+};
+
+int main()
+{
+	Something som;
+
+    return 0;
+}
