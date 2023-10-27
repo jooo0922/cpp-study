3fda0b52d6c070c7cd0c248c97af676b29db0cef
jooo0922 <jooo0922@gmail.com>
2023-10-27 23:33:02 +0900

    feat: Chapter8_10 프로젝트 생성

---
+++ TBCppChapter8/Chapter8_10/Chapter8_10.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int generateID()
+{
+    static int s_id = 0;
+    return ++s_id;
+}
+
+int main()
+{
+    cout << generateID() << endl;
+    cout << generateID() << endl;
+    cout << generateID() << endl;
+    return 0;
+}
