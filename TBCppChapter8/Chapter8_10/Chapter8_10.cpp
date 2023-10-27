7537bfc57bfab1adb268374e6ea2d6db001a5721
jooo0922 <jooo0922@gmail.com>
2023-10-27 23:53:30 +0900

    feat: static const 및 static constexpr

--- TBCppChapter8/Chapter8_10/Chapter8_10.cpp
+++ TBCppChapter8/Chapter8_10/Chapter8_10.cpp
@@ -12,7 +12,15 @@ int generateID()
 class Something
 {
 public:
-    static int s_value; // = 1; // static 멤버변수는 초기화를 할 수 없다!
+    static int s_value; // = 1; // static 멤버변수는 클래스 내에서 초기화를 할 수 없다!
+    static const int sc_value = 1; // const static 멤버변수는 클래스 내에서 초기화가 가능하다! -> 왜냐? const 니까! 상수 멤버변수는 항상 선언과 동시에 초기화되어야 함!
+
+    // constexpr (const expression)
+    // 일반적인 const 와 달리, 항상 '리터럴 값'으로만 초기화되어야 함!
+    // 왜냐하면, const 중에서는 변수나 템플릿을 넣는 경우 런타임에 초기화될 수 있는 경우가 있음.
+    // 그러나, constexpr 은 컴파일 타임에 미리 초기화해버리는 것을 강제하기 위해
+    // 초기화 값으로 리터럴 값만 받음!
+    static constexpr int sce_value = 1;
 };
 
 // 정적 멤버변수를 초기화하려면 클래스 블록 외부에서 
@@ -20,6 +28,9 @@ public:
 // 또 주의해야 할 점이, 정적 멤버변수는 "define in cpp" 즉,
 // cpp 안에서 정의되는 것이 권장됨. 헤더파일에서 정의하려고 하면 컴파일 에러를 발생시킴!
 int Something::s_value = 1;
+
+// 반대로, const static 멤버변수는 클래스 외부에서 초기화 불가능!
+//int Something::sc_value = 1;
 
 int main()
 {
