9447b5a5df0c4e54118dccdb3207c360e15e0fe4
jooo0922 <jooo0922@gmail.com>
2023-10-29 23:43:00 +0900

    feat: 산술 연산자 오버로딩을 friend 멤버로 등록

--- Chapter9/Chapter9_01/Chapter9_01.cpp
+++ Chapter9/Chapter9_01/Chapter9_01.cpp
@@ -10,13 +10,21 @@ private:
 public:
 	Cents(int cents) { m_cents = cents; };
 	int getCents() const { return m_cents; }
+
+	// 연산자 오버로딩 프로토타입을 friend 멤버로 등록해놓으면, 해당 연산자 오버로딩 내에서 private 멤버에도 접근 가능!
+	// getCents() 같은 getter 를 사용할 필요가 없어져서 더 편함
+	friend Cents operator + (const Cents& c1, const Cents& c2)
+	{
+		// 또한, 이왕 friend 멤버로 등록해놓은 김에 구현부까지 클래스 블록 안으로 옮겨놓으면 더 보기 좋겠지
+		return Cents(c1.getCents() + c2.getCents());
+	};
 };
 
 // 이것이 Cents 인스턴스 간 + 연산자를 사용할 때 어떤 기능을 수행할 것인지 오버로딩한 '연산자 오버로딩'
-Cents operator + (const Cents& c1, const Cents& c2)
-{
-	return Cents(c1.getCents() + c2.getCents());
-}
+//Cents operator + (const Cents& c1, const Cents& c2)
+//{
+//	return Cents(c1.getCents() + c2.getCents());
+//}
 
 int main()
 {
@@ -26,7 +34,7 @@ int main()
 	// 두 개의 Cents 클래스 인스턴스를 더하고 싶다면 어떻게 할까?
 	// 즉, 내가 만든 자료형(user defined data type)을 원시 자료형처럼 연산하고 싶다면?
 	// 방법이야 여러 가지가 있겠지만, 여기에 가장 적합한 방법은 '산술 연산자 오버로딩'을 활용하는 것!
-	cout << (cents1 + cents2 + Cents(6)).getCents() << endl;
+	cout << (cents1 + cents2 + Cents(6) + Cents(10) + Cents(10)).getCents() << endl;
 
 	return 0;
 }
