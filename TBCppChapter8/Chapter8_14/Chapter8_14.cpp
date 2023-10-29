88c8a82deabc90a4036e13e61de50bb60341f9c5
jooo0922 <jooo0922@gmail.com>
2023-10-29 19:56:55 +0900

    feat: 예제코드 작성

--- TBCppChapter8/Chapter8_14/Chapter8_14.cpp
+++ TBCppChapter8/Chapter8_14/Chapter8_14.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 
+enum FruitType
+{
+    APPLE, BANANA, CHERRY,
+};
+
+class Fruit
+{
+private:
+	FruitType m_type;
+
+public:
+	Fruit(FruitType type) : m_type(type)
+	{};
+
+	FruitType getType() { return m_type; }
+};
+
 int main()
 {
+	Fruit apple(APPLE);
+
+	if (apple.getType() == APPLE)
+	{
+		std::cout << "Apple" << std::endl;
+	}
     return 0;
 }
