da4a17ea8fa91e31a6f40e1be9960b9887834f77
jooo0922 <jooo0922@gmail.com>
2023-10-29 20:05:35 +0900

    feat: 필요한 라이브러리 include

--- TBCppChapter8/Chapter8_15/Chapter8_15.cpp
+++ TBCppChapter8/Chapter8_15/Chapter8_15.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
-
+#include <vector> // 동적 배열 사용을 위해 포함
+#include <algorithm> // sorting 함수 사용을 위해 포함
+#include <random> // random number 생성을 위해 포함
+#include <chrono> // 시간 측정을 위한 함수를 제공하는 표준 라이브러리
+ 
 int main()
 {
     return 0;
