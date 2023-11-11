ca328cd279462ecf851acdc6aa4500b33d2d17f7
jooo0922 <jooo0922@gmail.com>
2023-11-12 00:20:40 +0900

    feat: explicit 키워드를 생성자 함수에 명시

--- Chapter9/Chapter9_10/Chapter9_10.cpp
+++ Chapter9/Chapter9_10/Chapter9_10.cpp
@@ -10,7 +10,11 @@ private:
 	int m_denominator; // 분모 멤버
 
 public:
-	Fraction(int num = 0, int den = 1)
+	/* explicit 키워드 */
+	// 만약, 아래 main 함수의 예제 코드에서 살펴본 Converting constructor 기능을 막아버리고 싶다면,
+	// 생성자 앞에 'explicit' 키워드를 붙여주면 됨! explicit == '명시적인, 엄격한' 이라는 뜻을 갖고 있는 키워드로써,
+	// 말 그대로, 암시적으로 변환하지 말고, 명시적으로, 더 엄격하게 생성자 함수를 호출하라는 의미!
+	explicit Fraction(int num = 0, int den = 1)
 		: m_numerator(num), m_denominator(den)
 	{
 		// 분몯가 0 으로 입력될 것을 대비해서 assert() 를 사용하면 추후 디버깅에 아주 용이해 질 것임. 
@@ -56,7 +60,12 @@ int main()
 	// 이때, 컴파일러가 doSomething() 의 매개변수는 Fraction 타입밖에 없는데, 7이 전달되었다고 하면,
 	// 그냥 알아서 7을 매개변수로 선언된 Fraction frac 의 생성자 함수의 매개변수로 전달해버림.
 	// -> 이렇게 자동으로 변환해주는 것을 'Converting Cosntructor' 라고 함
-	doSomething(7);
+	//doSomething(7); // 생성자 함수에 explicit 키워드를 붙일 시, 컴파일 에러가 발생함
+
+	// explicit 키워드가 붙은 생성자 함수일 경우, 아래와 같이 명시적으로 생성자 함수를 호출하거나,
+	// 이미 생성된 인스턴스를 전달하는 방법을 사용해야겠지!
+	doSomething(frac);
+	doSomething(Fraction(7));
 
 	return 0;
 }
