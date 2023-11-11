f0c5397dab5fe6ff4a40cee7f96bfaa6953584dd
jooo0922 <jooo0922@gmail.com>
2023-11-11 23:29:52 +0900

    feat: int 형변환 연산자 오버로딩

--- Chapter9/Chapter9_08/Chapter9_08.cpp
+++ Chapter9/Chapter9_08/Chapter9_08.cpp
@@ -15,6 +15,14 @@ public:
 
 	int getCents() { return m_cents; }
 	void setCents(int cents) { m_cents = cents; }
+
+	/* 형변환 연산자 int 오버로딩 */
+	// 아래와 같이 선언하면, Cents 타입의 클래스를 int 타입으로 형변환하는 연산자 'int' 를 오버로딩 하는 것! 
+	operator int()
+	{
+		cout << "cast here" << endl;
+		return m_cents;
+	}
 };
 
 void printInt(const int& value)
@@ -26,9 +34,14 @@ int main()
 {
 	Cents cents(7);
 
-	// cents 는 Cents 클래스 타입이므로 int 타입의 입력 매개변수를 전달받는 printInt 함수에 암시적 형변환이 안되고 있음.
-	// 이러한 형변환을 오버로딩 할 수도 있다!
-	//printInt(cents);
+	// 이렇게 int 형변환 연산자를 사용하면 Cents 클래스에 정의된 int 형변환 연산자 오버로딩 내용이 실행된다!
+	int value = (int)cents;
+	value = int(cents);
+	value = static_cast<int>(cents);
+
+	// cents 는 Cents 클래스 타입이므로 int 타입의 입력 매개변수를 전달받는 printInt 함수에 암시적 형변환(= 자동 형변환)이 안되고 있음.
+	// 이 때, Cents 클래스를 int 타입으로 형변환 시켜주는 'int' 연산자에 대해 오버로딩하면, 클래스를 암시적 형변환하는 것도 커스터마이징이 가능하다!
+	printInt(cents);
 
 	return 0;
 }
