921c5eec5158a83d4668a4256077b591477ebd7d
jooo0922 <jooo0922@gmail.com>
2023-11-11 23:37:03 +0900

    feat: 클래스 간 형변환 연산자 오버로딩

--- Chapter9/Chapter9_08/Chapter9_08.cpp
+++ Chapter9/Chapter9_08/Chapter9_08.cpp
@@ -25,6 +25,23 @@ public:
 	}
 };
 
+// Dollar 클래스
+class Dollar
+{
+private:
+	int m_dollars = 0;
+
+public:
+	Dollar(const int& input) : m_dollars(input) {};
+	
+	// 이번에는 Cents 타입 클래스를 Dollar 타입 클래스로 형변환하는, 즉 형변환 오버로딩을 화폐 단위를 변환에 응용해보자!
+	operator Cents()
+	{
+		// 1 달러는 100 센트니까 현재 m_dollars 멤버에 100배를 곱한 값으로 Cents 인스턴스를 생성하여 반환
+		return Cents(m_dollars * 100); 
+	}
+};
+
 void printInt(const int& value)
 {
 	cout << value << endl;
@@ -42,6 +59,17 @@ int main()
 	// cents 는 Cents 클래스 타입이므로 int 타입의 입력 매개변수를 전달받는 printInt 함수에 암시적 형변환(= 자동 형변환)이 안되고 있음.
 	// 이 때, Cents 클래스를 int 타입으로 형변환 시켜주는 'int' 연산자에 대해 오버로딩하면, 클래스를 암시적 형변환하는 것도 커스터마이징이 가능하다!
 	printInt(cents);
+
+	
+	/* 클래스 간 형변환 오버로딩 */
+
+	Dollar dol(2);
+
+	// 타입이 다른 클래스 변수에 인스턴스를 할당할 경우, 암시적 형변환에 의해 
+	// Dollar 클래스 내에 정의된 Cents 형변환 연산자의 오버로딩이 실행됨
+	Cents cents2 = dol; 
+
+	printInt(cents2);
 
 	return 0;
 }
