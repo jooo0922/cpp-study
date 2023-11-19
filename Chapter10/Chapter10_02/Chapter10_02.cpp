a18fdb3d8683a617b1749b39460c636e470ab361
jooo0922 <jooo0922@gmail.com>
2023-11-19 13:35:39 +0900

    feat: 몬스터 위치 이동 및 몬스터 정보 출력

--- Chapter10/Chapter10_02/Chapter10_02.cpp
+++ Chapter10/Chapter10_02/Chapter10_02.cpp
@@ -1,8 +1,21 @@
-#include <iostream>
+//#include <iostream> // Monster.h 에서 이미 include 하고 있으므로 중복 include 불필요
+#include "Monster.h" // Monster 클래스 생성을 위해 포함
 
 using namespace std;
 
 int main()
 {
+    Monster mon1("Sanson", 0, 0);
+    
+    //while (1) // game loop
+    {
+        // event
+        // 몬스터 위치 이동
+        mon1.moveTo(1, 1);
+
+        // 몬스터 정보를 출력 스트림 오버로딩 함수를 통해 출력
+        cout << mon1 << endl;
+    }
+    
     return 0;
 }
