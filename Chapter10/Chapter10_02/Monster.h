4723915473752d2da1fabdd84819499d297fdeed
jooo0922 <jooo0922@gmail.com>
2023-11-19 13:35:27 +0900

    feat: 몬스터 위치 변경 멤버함수 및 출력 스트림 오버로딩

--- Chapter10/Chapter10_02/Monster.h
+++ Chapter10/Chapter10_02/Monster.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <iostream> // 출력 스트림 연산자 오버로딩을 위해 포함
 #include <string>
 
 // Monster 클래스 구현
@@ -14,4 +15,19 @@ public:
 	Monster(const std::string name_in, const int& x_in, const int& y_in)
 		: m_name(name_in), m_x(x_in), m_y(y_in)
 	{}
+
+	// 몬스터 위치 변경 멤버 함수
+	void moveTo(const int& x_target, const int& y_target)
+	{
+		m_x = x_target;
+		m_y = y_target;
+	}
+
+	// 출력 스트림 연산자 오버로딩을 친구 함수로 등록해서 구현
+	friend std::ostream& operator << (std::ostream& out, const Monster& monster)
+	{
+		out << monster.m_name << " " << monster.m_x << " " << monster.m_y << std::endl;
+
+		return out;
+	}
 };
