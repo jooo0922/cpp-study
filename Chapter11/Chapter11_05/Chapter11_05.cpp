086499d289f3bd82e77defe6b3d139cf2a937069
jooo0922 <jooo0922@gmail.com>
2023-12-06 00:25:21 +0900

    feat: protected 멤버를 자식 클래스에서 접근 테스트

--- Chapter11/Chapter11_05/Chapter11_05.cpp
+++ Chapter11/Chapter11_05/Chapter11_05.cpp
@@ -14,11 +14,30 @@ private:
 	int m_private;
 };
 
+class Derived : public Base
+{
+public:
+	Derived()
+	{
+		m_public = 123;
+
+		// protected 접근 지정자는 상속받은 자식 클래스에서는 접근 가능!
+		m_protected = 123;
+
+		// private 접근 지정자는 상속받은 자식 클래스에서 조차 접근 불가!
+		//m_private = 123;
+	}
+};
+
 int main()
 {
 	Base base;
 
 	base.m_public = 123;
+
+	// 아래와 같이, protected, private 접근 지정자로 선언된 멤버에는 접근 불가!
+	/*base.m_protected = 123; 
+	base.m_private = 123; */
 
     return 0;
 }
