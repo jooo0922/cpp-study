13df4a0a8b7f418cebad6a15f152fa28deccd6c7
jooo0922 <jooo0922@gmail.com>
2023-12-06 00:38:33 +0900

    feat: private inherit 테스트

--- Chapter11/Chapter11_05/Chapter11_05.cpp
+++ Chapter11/Chapter11_05/Chapter11_05.cpp
@@ -15,7 +15,8 @@ private:
 };
 
 //class Derived : public Base
-class Derived : protected Base
+//class Derived : protected Base // protected inherit
+class Derived : private Base // private inherit -> 얘는 자식 클래스 내부보다는, 자식 클래스를 상속받는 "손자 클래스" 에서 차이점을 확인 가능! 
 {
 public:
 	Derived()
@@ -33,6 +34,21 @@ public:
 		Base::m_public; // 이는 this->m_public; m_public; 으로 선언한 것과 동일함. 부모로부터 상속받은 멤버임을 강조하기 위함!
 		Base::m_protected;
 		//Base::m_private; // 이건 protected inherit 으로 상속 받을 때에도 접근 불가!
+	}
+};
+
+// private inherit 으로 상속된 Derived 클래스를 상속받는 손자 클래스
+class GrandChild : public Derived
+{
+public:
+	GrandChild()
+	{
+		// Derived 클래스는 private inherit 으로 상속을 받았기 때문에,
+		// 현재 Derived 클래스에 존재하는 m_public 멤버는 private 으로 캡술화되어 있는 것과 같은 상태임.
+		// 즉, Derived 에 상속된 멤버들은 '최소한 private 보다 엄격하게' 관리되고 있는 것!
+		// 따라서, Derived 를 상속받은 손자 클래스 GrandChild 는 public 멤버였던 m_public 조차 내부에서 접근 불가!
+		/*Derived::m_public;
+		Derived::m_protected;*/
 	}
 };
 
