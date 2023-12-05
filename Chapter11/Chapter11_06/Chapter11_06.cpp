82d2919e537b0d1b27cdf9fb9702d56e9d55c118
jooo0922 <jooo0922@gmail.com>
2023-12-06 00:45:45 +0900

    chore: Chapter11_06 프로젝트 생성

---
+++ Chapter11/Chapter11_06/Chapter11_06.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+class Base
+{
+private:
+    int m_value;
+
+public:
+    Base(int value)
+        : m_value(value)
+    {
+
+    }
+};
+
+class Derived : public Base
+{
+public:
+    // 항상 멤버 초기화 리스트 문법에는 
+    // 부모클래스 생성자 함수 호출 코드가 내부적으로 숨어있는 거라고 했었지? -> 그래서 부모클래스 생성자가 먼저 호출될 수 있었던 것!
+    // https://github.com/jooo0922/cpp-study/blob/main/Chapter11/Chapter11_03/Chapter11_03.cpp 참고!
+    Derived(int value)
+        : Base(value) 
+    {
+
+    }
+};
+
+int main()
+{
+    return 0;
+}
