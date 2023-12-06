e0dad627e1f716d5657038c0fd4b1135c0a08afa
jooo0922 <jooo0922@gmail.com>
2023-12-06 23:58:47 +0900

    feat: 상속받은 함수 오버라이딩

--- Chapter11/Chapter11_07/Chapter11_07.cpp
+++ Chapter11/Chapter11_07/Chapter11_07.cpp
@@ -11,6 +11,11 @@ public:
     Base(int value)
         : m_i(value)
     {}
+
+    void print()
+    {
+        cout << "I'm base " << endl;
+    }
 };
 
 class Derived : public Base
@@ -25,9 +30,37 @@ public:
     Derived(int value)
         : Base(value)
     {}
+
+    /*
+        어떤 경우에는, 부모 클래스의 기능을 그대로 수행하되,
+        자식 클래스에서 추가적인 기능을 더 수행하고 싶은 경우가 있음.
+
+        또한, 함수의 이름마저 동일하게 유지하고 싶을 수 있겠지?
+
+        이럴 경우, 클래스의 다형성을 활용할 수도 있겠지만, 
+        그건 다음 챕터에 배울 것이고,
+
+        여기서 소개할 방법은 '상속받은 함수 오버라이딩'으로,
+        상속받은 부모 클래스의 동일한 이름을 갖는 멤버 함수를 먼저 호출해주면 됨!
+
+        <부모클래스::멤버함수()>
+        형태로 영역표시 연산자(::) 를 사용해서
+        부모클래스의 원본 멤버함수에 접근하여 실행하면 됨!
+    */
+    void print()
+    {
+        Base::print();
+        cout << "I'm derived " << endl;
+    }
 };
 
 int main()
 {
+    Base base(5);
+    base.print();
+
+    Derived derived(7);
+    derived.print();
+
     return 0;
 }
