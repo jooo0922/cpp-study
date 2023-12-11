107201025ec447b7432e11975e9a9d1e6f4de0a2
jooo0922 <jooo0922@gmail.com>
2023-12-12 00:07:33 +0900

    feat: 자식클래스 주소값을 부모클래스 포인터에 캐스팅

--- Chapter12/Chapter12_01/Chapter12_01.cpp
+++ Chapter12/Chapter12_01/Chapter12_01.cpp
@@ -56,5 +56,21 @@ int main()
     Cat cat("my cat");
     Dog dog("my dog");
 
+    // 각 클래스의 speak() 멤버함수 실행
+    animal.speak();
+    cat.speak();
+    dog.speak();
+
+    // 이번에는 Animal* (부모클래스)타입의 포인터 변수에 Cat, Dog (자식클래스) 인스턴스 주소값을 저장해보자
+    Animal* ptr_animal1 = &cat;
+    Animal* ptr_animal2 = &dog;
+
+    // 그 다음, 각 부모클래스 타입 포인터 변수로부터 speak() 를 실행해보면?
+    // 자식클래스 인스턴스의 주소값을 부모클래스 타입의 주소값으로 casting 한 다음, 
+    // casting 된 주소값이 담긴 포인터 변수를 통해 호출시키면, 자기가 부모클래스인 줄 알고 작동을 함!
+    ptr_animal1->speak();
+    ptr_animal2->speak();
+
+
     return 0;
 }
