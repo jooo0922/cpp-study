b72b9cf5eed4fe62afa8d6b6145c042aa3465e43
jooo0922 <jooo0922@gmail.com>
2023-12-12 00:19:38 +0900

    feat: virtual 로 멤버함수 다형성 구현

--- Chapter12/Chapter12_01/Chapter12_01.cpp
+++ Chapter12/Chapter12_01/Chapter12_01.cpp
@@ -17,7 +17,8 @@ public:
 
     // 상수 멤버함수로 선언 -> 클래스 인스턴스 데이터 불변을 보장
     // const 인스턴스에서 사용 가능
-    void speak() const
+    // 'virtual' 키워드를 붙임으로써, 이 멤버함수는 '가상함수'가 됨.
+    virtual void speak() const
     {
         cout << m_name << " ??? " << endl;
     }
@@ -71,6 +72,30 @@ int main()
     ptr_animal1->speak();
     ptr_animal2->speak();
 
+
+    /* 위 성질을 활용할 수 있는 케이스 */
+
+    // 각각의 자식클래스 정적 배열 초기화
+    Cat cats[] = { Cat("cat1"), Cat("cat2"), Cat("cat3"), Cat("cat4"), Cat("cat5") };
+    Dog dogs[] = { Dog("dog1"), Dog("dog2") };
+
+    // 여기서, 두 배열에 대해서 반복문을 순회하고 싶다면?
+    // 일반적으로는 각 배열에 대해서 for 문을 별도로 구현하고 싶겠지만,
+    // Cat, Dog 모두 Animal 클래스로부터 상속받은 자식클래스니까, Animal 클래스 포인터로 한번에 묶어버려서
+    // 반복문을 한 번에 돌리도록 처리할 수도 있지 않을까?
+    Animal* my_animals[] = { &cats[0], &cats[1], &cats[2], &cats[3], &cats[4],
+                             &dogs[0], &dogs[1] };
+
+    // Animal 클래스 포인터로 묶어버린 정적 배열에 대해서 반복문을 한꺼번에 순회
+    for (int i = 0; i < 7; i++)
+    {
+        // 그런데, Animal 클래스 포인터로 묶은 다음 speak() 를 실행하면 어차피 " ??? " 만 호출될텐데
+        // 배열 하나로 묶어봤자 뭔 소용일까? -> 이럴 때, 부모클래스의 멤버함수 speak() 를 virtual(가상함수)로 만들어주면 됨!
+        // -> 이렇게 함으로써, 부모클래스 타입의 포인터로 casting 된 자식클래스 인스턴스들이
+        // 실제로도 '자식클래스인 것 처럼' 작동할 수 있게 됨! 
+        // -> 이러한 성질을 '다형성' 이라고 함! 
+        my_animals[i]->speak();
+    }
 
     return 0;
 }
