47e61643b6e8aa71c09ec514c347f916bcf68ff9
jooo0922 <jooo0922@gmail.com>
2023-12-12 00:24:53 +0900

    feat: 4단계 상속 구조 구현

--- Chapter12/Chapter12_02/Chapter12_02.cpp
+++ Chapter12/Chapter12_02/Chapter12_02.cpp
@@ -8,6 +8,24 @@ public:
     void print() { cout << "A" << endl; }
 };
 
+class B : public A
+{
+public:
+    void print() { cout << "B" << endl; }
+};
+
+class C : public B
+{
+public:
+    void print() { cout << "C" << endl; }
+};
+
+class D : public C
+{
+public:
+    void print() { cout << "D" << endl; }
+};
+
 int main()
 {
     return 0;
