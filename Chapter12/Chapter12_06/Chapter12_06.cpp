0942a4a3d137a8602035c04f6b4e4e65bb9a74e7
jooo0922 <jooo0922@gmail.com>
2023-12-21 00:06:59 +0900

    feat: 가상테이블 주소값 생성에 의한 클래스 크기 변화 확인

--- Chapter12/Chapter12_06/Chapter12_06.cpp
+++ Chapter12/Chapter12_06/Chapter12_06.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+using namespace std;
+
 // 부모 클래스
 class Base
 {
@@ -8,7 +10,7 @@ public:
     // -> 가상함수 선언 시, "동적 바인딩"으로 가상함수를 관리하므로, 
     // 가상테이블을 가리키는 주소값이 생성됨!
     // (함수 동적 바인딩 관련 https://github.com/jooo0922/cpp-study/blob/main/Chapter12/Chapter12_05/Chapter12_05.cpp 참고)
-    void fun1() {};
+    virtual void fun1() {};
     void fun2() {};
 };
 
@@ -24,5 +26,12 @@ public:
 
 int main()
 {
+    // 부모클래스에서 가상함수(virtual)이 하나라도 선언되었다면,
+    // 가상테이블을 가리키는 주소값이 내부적으로 생성됨에 따라, 
+    // 클래스의 크기가 1 byte -> 4 bytes(x86 기준) or 8 bytes(x64 기준) 로 변경됨.
+    // 참고로, x86 에서 메모리 주소값은 4 bytes, x64 에서는 8 bytes 로 표현된다고 했었지?
+    cout << sizeof(Base) << endl;
+    cout << sizeof(Der) << endl;
+
     return 0;
 }
