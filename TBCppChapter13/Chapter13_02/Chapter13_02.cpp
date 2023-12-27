c3c37ad5557dbdba5d25f60469ca051e80239314
jooo0922 <jooo0922@gmail.com>
2023-12-28 00:26:56 +0900

    feat: 클래스를 클래스 템플릿으로 변형

--- TBCppChapter13/Chapter13_02/Chapter13_02.cpp
+++ TBCppChapter13/Chapter13_02/Chapter13_02.cpp
@@ -2,12 +2,23 @@
 
 int main()
 {
-    MyArray my_array(10);
+	/*
+		클래스 템플릿을 사용할 경우,
+		해당 클래스 타입의 변수 및 인스턴스를 선언할 때,
+
+		꺽쇠 <> 안에
+		'template argument list'
+		라고 하는, 
+		
+		이 클래스가 Template Instantiation 할
+		구체적인 타입을 지정해줘야 함!
+	*/
+    MyArray<double> my_array(10);
 
 	// MyArray 클래스의 배열 요소들 할당
 	for (int i = 0; i < my_array.getLength(); i++)
 	{
-		my_array[i] = i * 10;
+		my_array[i] = i * 0.5;
 	}
 
 	// MyArray 인스턴스의 각 요소들 출력
--- TBCppChapter13/Chapter13_02/MyArray.h
+++ TBCppChapter13/Chapter13_02/MyArray.h
@@ -5,11 +5,39 @@
 
 using namespace std;
 
+/*
+	만약, 클래스에서도
+	각 멤버변수의 built-in 타입들을
+	템플릿으로 지정해서 
+
+	컴파일러가 알아서 Template Instantiation 을
+	처리해줄 수 있을까? 물론 가능함!
+
+	참고로, 템플릿에서 'typename' 키워드 대신,
+	클래스 이름을 작성하는 것도 가능함.
+
+	그러나, 템플릿에서는 항상
+	클래스같은 user-defined 자료형만 Instantiation 하는 것은 아니므로,
+
+	좀 더 일반적인 built-in 자료형까지 포괄할 수 있도록
+	'typename' 키워드를 사용하는 것을 권장함!
+*/
+
+template<typename T>
+
 class MyArray
 {
 private:
 	int m_length; // 배열 크기
-	int* m_data; // 배열 첫 번째 요소 포인터 멤버변수
+
+	// 배열 첫 번째 요소 포인터 멤버변수
+	/*
+		이렇게 템플릿으로 타입을 지정하면,
+
+		'이 정적 배열은 int, float, class 등 어떤 타입이라도 가능하다'
+		라고 선언한 것으로 보면 됨!
+	*/
+	T* m_data; 
 
 public:
 	// 생성자 함수
@@ -24,7 +52,7 @@ public:
 	MyArray(int length)
 	{
 		// 배열 힙 메모리 동적 할당
-		m_data = new int[length];
+		m_data = new T[length];
 		m_length = length;
 	}
 
@@ -43,7 +71,7 @@ public:
 	}
 
 	// [] 연산자 오버로딩
-	int& operator[](int index)
+	T& operator[](int index)
 	{
 		// assert() 함수로 입력받은 index 가 유효한 지 검사
 		assert(index >= 0 && index < m_length);
