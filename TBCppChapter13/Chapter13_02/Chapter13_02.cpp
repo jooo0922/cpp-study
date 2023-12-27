fa2a8e19affbecffad318a33d4b7d71307fc5a42
jooo0922 <jooo0922@gmail.com>
2023-12-28 00:17:26 +0900

    feat: MyArray 클래스 인스턴스 생성 및 테스트 코드 작성

--- TBCppChapter13/Chapter13_02/Chapter13_02.cpp
+++ TBCppChapter13/Chapter13_02/Chapter13_02.cpp
@@ -2,5 +2,16 @@
 
 int main()
 {
+    MyArray my_array(10);
+
+	// MyArray 클래스의 배열 요소들 할당
+	for (int i = 0; i < my_array.getLength(); i++)
+	{
+		my_array[i] = i * 10;
+	}
+
+	// MyArray 인스턴스의 각 요소들 출력
+	my_array.print();
+
     return 0;
 }
