9471be32241f2659936a59c0cab4709a22447c80
jooo0922 <jooo0922@gmail.com>
2024-01-02 14:13:54 +0900

    feat: 멤버함수에 전달하는 파라미터로 템플릿 파라미터 암묵적 지정

--- TBCppChapter13/Chapter13_08/Chapter13_08.cpp
+++ TBCppChapter13/Chapter13_08/Chapter13_08.cpp
@@ -20,7 +20,7 @@ public:
     // 이미 템플릿화된 클래스 내의
     // 특정 멤버함수를 '한 번 더 템플릿화' 하기
     template <typename TT>
-    void doSomething()
+    void doSomething(const TT& input)
     {
         // 클래스의 템플릿 파라미터와 멤버함수의 템플릿 파라미터를 출력함
         cout << typeid(T).name() << " " << typeid(TT).name() << endl;
@@ -40,7 +40,10 @@ int main()
 
     // 한 번 더 템플릿화 된 멤버함수를 호출할 경우,
     // 템플릿 파라미터(TT)를 추가로 지정해서 전달해줘야 함.
-    a_int.doSomething<float>();
+    //a_int.doSomething<float>();
+
+    // 멤버함수에 전달하는 파라미터 자체로 템플릿 파라미터를 '암묵적으로' 지정할 수 있음!
+    a_int.doSomething(123.4); // 멤버함수의 템플릿 파라미터 TT 가 '암묵적으로' double 로 지정됨! 
 
     return 0;
 }
