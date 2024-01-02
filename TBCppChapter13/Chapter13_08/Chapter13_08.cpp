81dc3703851e0f3042ac60e6f29ed28a1abf96b3
jooo0922 <jooo0922@gmail.com>
2024-01-02 14:16:58 +0900

    feat: 멤버함수 템플릿 파라미터 타입으로 형변환

--- TBCppChapter13/Chapter13_08/Chapter13_08.cpp
+++ TBCppChapter13/Chapter13_08/Chapter13_08.cpp
@@ -24,6 +24,10 @@ public:
     {
         // 클래스의 템플릿 파라미터와 멤버함수의 템플릿 파라미터를 출력함
         cout << typeid(T).name() << " " << typeid(TT).name() << endl;
+
+        // 이렇게 두 개의 템플릿 파라미터가 공존할 경우,
+        // 클래스의 템플릿 파라미터 타입(T)을 멤버함수의 템플릿 파라미터 타입(TT) 로 형변환(casting) 하는 것도 가능!
+        cout << (TT)m_value << endl;
     }
 
     // 멤버변수 출력 함수
@@ -35,15 +39,15 @@ public:
 
 int main()
 {
-    A<int> a_int(123);
-    a_int.print();
+    A<char> a_char('A');
+    a_char.print();
 
     // 한 번 더 템플릿화 된 멤버함수를 호출할 경우,
     // 템플릿 파라미터(TT)를 추가로 지정해서 전달해줘야 함.
-    //a_int.doSomething<float>();
+    //a_char.doSomething<float>();
 
     // 멤버함수에 전달하는 파라미터 자체로 템플릿 파라미터를 '암묵적으로' 지정할 수 있음!
-    a_int.doSomething(123.4); // 멤버함수의 템플릿 파라미터 TT 가 '암묵적으로' double 로 지정됨! 
+    a_char.doSomething(1); // 멤버함수의 템플릿 파라미터 TT 가 '암묵적으로' int 로 지정됨! 
 
     return 0;
 }
