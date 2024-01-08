057eaf5095ef817418c5c9007e07c87e006a0a5d
jooo0922 <jooo0922@gmail.com>
2024-01-08 22:25:01 +0900

    feat: 스마트 포인터를 활용한 예외처리 시 메모리 누수 방지

--- TBCppChapter14/Chapter14_06/Chapter14_06.cpp
+++ TBCppChapter14/Chapter14_06/Chapter14_06.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory> // 스마트 포인터 사용을 위해 포함
 
 using namespace std;
 
@@ -8,6 +9,24 @@ int main()
 	{
 		// 배열 메모리 동적 할당
 		int* i = new int[1000000];
+
+		/*
+			예외처리로 인한 메모리 누수 해결법 - 스마트 포인터!
+
+
+			unique_ptr<> 타입의 스마트 포인터를 사용해서
+			동적 할당된 포인터 변수를 매개변수로 전달하면,
+
+			현재 영역을 벗어날 시,
+			unique_ptr 이 알아서 동적 할당된 메모리를 반납해 줌.
+
+			더 좋은 것은,
+			throw 키워드로 인해
+			catch 문으로 날라갈 때에도
+
+			unique_ptr 이 알아서 메모리를 반납해 줌.
+		*/
+		unique_ptr<int> up_i(i);
 
 		// do something with i
 
@@ -27,7 +46,7 @@ int main()
 			왜냐하면, throw 이후 실행될 catch 문에서
 			포인터 변수 i 에 접근할 방법이 없기 때문...
 		*/
-		delete[] i;
+		//delete[] i;
 	}
 	catch (...)
 	{
