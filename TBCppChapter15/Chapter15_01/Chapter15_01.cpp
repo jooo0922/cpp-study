b8f4e7d72224c367eb2e8d4dfa0ac486a361353f
jooo0922 <jooo0922@gmail.com>
2024-01-20 20:51:33 +0900

    feat: RAII 구조 구현

--- TBCppChapter15/Chapter15_01/Chapter15_01.cpp
+++ TBCppChapter15/Chapter15_01/Chapter15_01.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include "Resource.h"
+
+using namespace std;
+
+// RAII : resource acquisition is initialization
+
+void doSomething()
+{
+    /*
+        new 키워드로 메모리를 동적 할당을 했으면,
+        delete 키워드로 반드시 메모리를 반납해야 한다.
+
+        -> 이러한 개념을 'RAII' 라고 함!
+    */
+    Resource* res = new Resource;
+
+    // work with res
+
+    delete res;
+
+    return;
+}
 
 int main()
 {
+    doSomething();
+
     return 0;
 }
