cf00be902952499a3c12c5a7f18f25d7bf9ef8c0
jooo0922 <jooo0922@gmail.com>
2024-01-29 01:55:56 +0900

    feat: shared_ptr 에 의한 메모리 소유권 순환 문제 재현

--- TBCppChapter15/Chapter15_07/Chapter15_07.cpp
+++ TBCppChapter15/Chapter15_07/Chapter15_07.cpp
@@ -55,5 +55,38 @@ public:
 
 int main()
 {
+    // 각 Person 클래스를 shared_ptr 스마트 포인터로 관리
+    auto lucy = std::make_shared<Person>("Lucy");
+    auto ricky = std::make_shared<Person>("Ricky");
+
+    // partnerUp 친구 함수를 실행해서 두 Person 클래스가 내부적으로
+    // 서로의 메모리를 shared_ptr 포인터로 공유할 수 있도록 함
+    /*
+        이 때, 두 Person 클래스 인스턴스가 
+        shared_ptr 로 공유되긴 하지만,
+        
+        main() block 이 종료가 되어도
+        소멸자 함수가 호출되지 않음.
+
+        즉, shared_ptr 이 사라지지 않아서
+        memory leak 이 발생하고 있는 상황이겠지!
+
+        왜 이런 일이 발생하는 걸까?
+
+        각각의 Person 을 관리하는
+        shared_ptr 인 lucy, ricky 를 지우려고 하더라도,
+        
+        이미 각 Person 내부에서 서로의 메모리 소유권을 공유하는
+        또 다른 shared_ptr 인 m_partner 가 존재하기 때문에,
+
+        모든 shared_ptr 이 사라질 수 없어서
+        Partner 클래스 자체의 메모리가 지워지지 못하고 있는 상태인 것!
+
+        -> 즉, 메모리 소유권이 '순환'되어서 꼬이게 된 것임!
+
+        -> 이를 해결하기 위해 사용하는 또 다른 스마트 포인터가 std::weak_ptr 임!
+    */
+    partnerUp(lucy, ricky);
+
     return 0;
 }
