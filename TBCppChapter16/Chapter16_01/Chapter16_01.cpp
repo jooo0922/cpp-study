3fa4e81af2ab600b929ea513481c9c4e37b462ee
jooo0922 <jooo0922@gmail.com>
2024-01-29 22:47:06 +0900

    feat: deque, set 예제 구현

--- TBCppChapter16/Chapter16_01/Chapter16_01.cpp
+++ TBCppChapter16/Chapter16_01/Chapter16_01.cpp
@@ -27,6 +27,55 @@ void sequence_containers()
         }
         cout << endl;
     }
+
+    // deque
+    {
+        deque<int> deq;
+        for (int i = 0; i < 10; i++)
+        {
+            /*
+                vector 가
+                요소들을 뒤에만 추가할 수 있는 것과 달리,
+
+                deque 는
+                요소들을 앞 / 뒤 모두에 추가할 수 있음!
+            */
+            deq.push_back(i);
+            deq.push_front(i);
+        }
+
+        for (auto& e : deq)
+        {
+            cout << e << " ";
+        }
+        cout << endl;
+    }
+}
+
+void associative_containers()
+{
+    // set
+    /*
+        영어 단어 뜻 그대로 '집합' 자료구조
+
+        중복된 원소를 허용하지 않음.
+        js 의 Set 과 유사한 자료구조
+    */
+    {
+        set<string> str_set;
+
+        str_set.insert("Hello");
+        str_set.insert("World");
+        str_set.insert("Hello"); // 중복된 원소 추가 시도
+
+        cout << str_set.size() << endl; // 중복된 원소를 제외한 set 의 크기는 2
+
+        for (auto& e : str_set)
+        {
+            cout << e << " ";
+        }
+        cout << endl;
+    }
 }
 
 int main()
@@ -36,6 +85,9 @@ int main()
 
     // sequence containers
     sequence_containers();
+
+    // associative containers
+    associative_containers();
 
     return 0;
 }
