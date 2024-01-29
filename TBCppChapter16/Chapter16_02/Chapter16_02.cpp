cf2ce42ef53426d95bc25f3de9f0cb1896a808ad
jooo0922 <jooo0922@gmail.com>
2024-01-29 23:22:23 +0900

    feat: vector 컨테이너의 반복자 사용 예제 구현

--- TBCppChapter16/Chapter16_02/Chapter16_02.cpp
+++ TBCppChapter16/Chapter16_02/Chapter16_02.cpp
@@ -8,5 +8,29 @@ using namespace std;
 
 int main()
 {
+    // vector 컨테이너 생성 후, 데이터 초기화
+    vector<int> container;
+    for (int i = 0; i < 10; i++)
+    {
+        container.push_back(i);
+    }
+
+    // vector 컨테이너의 각 요소들을 순회할 때 사용할 반복자 자체를 하나의 변수로 선언함
+    vector<int>::const_iterator itr;
+
+    // vector 컨테이너의 반복자를 초기화할 때에는, std::vector<>.begin() 을 사용함.
+    itr = container.begin();
+    
+    // 반복자가 container.end() 와 같아질 때까지 계속 순회함
+    while (itr != container.end())
+    {
+        // 반복자를 de-referencing 한 값을 출력 (de-referencing 연산자를 오버로딩해서 커스텀하게 무언가를 출력 가능)
+        cout << *itr << " ";
+
+        // 반복자를 증가 연산자로 증가시킴
+        ++itr;
+    }
+    cout << endl;
+
     return 0;
 }
