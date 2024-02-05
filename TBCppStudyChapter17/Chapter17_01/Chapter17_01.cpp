f693079795b0353896db363e4f3fe618af06ed1c
jooo0922 <jooo0922@gmail.com>
2024-02-05 23:07:21 +0900

    feat: c-style 문자열 처리 예제 구현

--- TBCppStudyChapter17/Chapter17_01/Chapter17_01.cpp
+++ TBCppStudyChapter17/Chapter17_01/Chapter17_01.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <string> // std::string 클래스를 사용하기 위해 포함
+#include <cstddef>
+#include <locale>
 
 using namespace std;
 
 int main()
 {
+    // c-style string example -> 모든 문자열을 이렇게 다루기에는 너무 번거로움.
+    {
+        char* strHello = new char[7];
+        strcpy_s(strHello, sizeof(char) * 7, "hello!");
+        std::cout << strHello << std::endl;
+    }
+
     return 0;
 }
