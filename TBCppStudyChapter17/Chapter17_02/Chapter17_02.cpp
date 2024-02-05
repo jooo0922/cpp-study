502d3bee9e74c1e7142a0bc6b905410ee045a47d
jooo0922 <jooo0922@gmail.com>
2024-02-06 00:05:46 +0900

    feat: ToString() 함수 템플릿 구현

--- TBCppStudyChapter17/Chapter17_02/Chapter17_02.cpp
+++ TBCppStudyChapter17/Chapter17_02/Chapter17_02.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <sstream> // string stream 의 약자. 문자열 입출력 스트림을 사용하기 위해 포함
 
 using namespace std;
+
+/*
+    어떤 타입의 값이던지 문자열로 변환해주는 함수 구현
+*/
+template <typename T>
+std::string ToString(T x)
+{
+    // 문자열 출력 스트림을 하나 만든다.
+    std::ostringstream osstream;
+
+    // 매개변수로 들어온 x 의 타입이 뭐가 됐건 문자열 출력 스트림에 넣어버림.
+    osstream << x;
+
+    /*
+        std::ostringstream.str() 은
+        출력 스트림 연산자로 들어온 값의 타입이 뭐가 됐건,
+        무조건 문자열로 변환해서 던져줌.
+    */
+    return osstream.str();
+}
 
 int main()
 {
@@ -30,7 +51,10 @@ int main()
     cout << second_string << endl;
 
     // 숫자 -> 문자로 변환한 후, std::string 생성자 매개변수에 전달하는 트릭도 알아둘 것!
-    string my_str(std::to_string(1004));
+    //string my_str(std::to_string(1004));
+
+    // 문자열 출력 스트림 std::ostringstream 을 사용해서 문자열 변환하기
+    string my_str(ToString(3.141592));
 
     // 변환된 문자를 다시 int 또는 float 타입으로 되돌리기
     int i = std::stoi(my_str);
@@ -39,7 +63,8 @@ int main()
     cout << f << endl;
 
     // 변환된 문자를 뒤에 이어붙이는 것도 가능!
-    my_str += std::to_string(128);
+    //my_str += std::to_string(128);
+
     cout << my_str << endl;
 
     return 0;
