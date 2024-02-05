bdca5580249786d4a37233ed02e987f79ab2e0cc
jooo0922 <jooo0922@gmail.com>
2024-02-05 23:52:11 +0900

    feat: STL 컨테이너를 활용한 문자열 매개변수 전달

--- TBCppStudyChapter17/Chapter17_02/Chapter17_02.cpp
+++ TBCppStudyChapter17/Chapter17_02/Chapter17_02.cpp
@@ -12,10 +12,21 @@ int main()
     */
 
     //string my_string; // default constructor (기본 생성자)
-    string my_string("my string");
-    string second_string(my_string); // copy constructor (복사 생성자)
+    //string my_string("my string");
+    const char* my_string = "my string"; // c-style string 도 매개변수로 전달 가능
 
-    cout << my_string << endl;
+    // STL 컨테이너의 한 종류인 std::vector<> 자체를 문자열 매개변수로 전달하는 것도 가능함!
+    vector<char> vec;
+    for (auto e : "Today is a good day.")
+    {
+        vec.push_back(e);
+    }
+    string second_string(vec.begin(), vec.end()); // char 타입 문자가 추가된 컨테이너의 반복자를 매개변수로 전달
+
+    //string second_string(my_string); // copy constructor (복사 생성자)
+    //string second_string(my_string, 3, 5); // 출력 결과 : strin -> 맨 앞의 문자 3개를 자르고, 거기서부터 5개 까지의 글자만 가져옴.
+    //string second_string(10, 'A'); // 출력 결과 : AAAAAAAAAA -> 두 번째 매개변수로 전달된 문자열을 10번 출력함.
+
     cout << second_string << endl;
 
     return 0;
