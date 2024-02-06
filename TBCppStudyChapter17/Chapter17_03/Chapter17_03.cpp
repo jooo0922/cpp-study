2912c6baecc50d1ac1dcc2bbd5cad4976730dab1
jooo0922 <jooo0922@gmail.com>
2024-02-06 23:26:13 +0900

    feat: std::string.capacity() 로 문자열 용량 출력

--- TBCppStudyChapter17/Chapter17_03/Chapter17_03.cpp
+++ TBCppStudyChapter17/Chapter17_03/Chapter17_03.cpp
@@ -19,12 +19,42 @@ int main()
         이를 std::string.size() 함수를 통해 가져올 수 있음.
     */
     cout << my_str.size() << endl; // 9
+    cout << my_str.length() << endl; // 9
+
 
     /*
-        std::string 에 문자열이 비어있는지 여부를 출력
+        std::string.empty() 를 통해 문자열이 비어있는지 여부를 출력
     */
     cout << std::boolalpha; // 'true' 또는 'false' 로 출력되도록 설정
     cout << my_str.empty() << endl; // false
+
+
+    /*
+        std::string.capacity() 를 통해 문자열의 '용량' 출력
+
+        왜 .size() 또는 .length() 와 
+        .capacity() 값이 차이가 날까?
+
+        std::string 에서는 내부적으로 
+        new, delete 등의 키워드에 의한 메모리 재할당을 최소화하고자 함.
+
+        왜냐하면, memory reallocation(재할당)는
+        속도가 느려지고 성능이 저하되는 주된 원인이므로,
+        
+        처음부터 메모리를 여유롭게 잡아놓고,
+        추후 문자열이 추가되거나 변경되면,
+
+        미리 여유롭게 잡아놓은 메모리 덕분에
+        new, delete 등의 키워드로 불필요한 메모리 재할당 과정을 거치지 않아도 됨!
+
+        그러다 보니,
+        실제 문자열 길이보다
+        메모리 용량을 여유롭게 잡아놓기 때문에
+
+        .capacity() 로 반환받는 값이
+        .size(), .length() 로 반환받는 값보다 더 크게 출력되는 것임.
+    */
+    cout << my_str.capacity() << endl; // 15
 
     return 0;
 }
