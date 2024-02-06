201dd7fc06787c5aa51db992eb568a3cbfe33a15
jooo0922 <jooo0922@gmail.com>
2024-02-06 23:50:52 +0900

    feat: std::string 의 각 문자에 접근하기

--- TBCppStudyChapter17/Chapter17_04/Chapter17_04.cpp
+++ TBCppStudyChapter17/Chapter17_04/Chapter17_04.cpp
@@ -6,5 +6,19 @@ using namespace std;
 
 int main()
 {
+    string my_str("abcdefg");
+
+    /*
+        std::string 내에 정의된 
+        [] 연산자 오버로딩을 사용하여
+        문자열의 각 문자에 접근하기
+    */
+    cout << my_str[0] << endl;
+    cout << my_str[3] << endl;
+
+    // 특정 인덱스의 문자를 바꿔치기 하는 것도 가능!
+    my_str[3] = 'Z';
+    cout << my_str << endl;
+
     return 0;
 }
