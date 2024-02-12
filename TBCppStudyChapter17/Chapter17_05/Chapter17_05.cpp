0f50b9eb6ac0e549188bf19955533e672f217bda
jooo0922 <jooo0922@gmail.com>
2024-02-12 12:53:56 +0900

    feat: std::string.assign(), std::string.append() 사용 예제

--- TBCppStudyChapter17/Chapter17_05/Chapter17_05.cpp
+++ TBCppStudyChapter17/Chapter17_05/Chapter17_05.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 int main()
 {
+    string str1("one");
+
+    string str2;
+    str2 = str1;
+    str2 = "two";
+
+    /*
+        std::string.assign(const char*)
+
+        c-style 문자열을 할당하는 기능!
+
+        마우스를 갖다 대보면,
+        반환값이 std::string 자기 자신에 대한 참조변수인
+        std::string& 로 나옴.
+
+        즉, method chaining 을 사용할 수 있다는 뜻!
+
+        .append() 같은 std::string 메서드를 연달아 사용 가능!
+
+        참고로,
+        std::string.append(const char*)는
+        뒤에 문자열을 추가로 덧붙이는 기능!
+    */
+    str2.assign("two").append(" ").append("three");
+
+    cout << str2 << endl;
+
     return 0;
 }
