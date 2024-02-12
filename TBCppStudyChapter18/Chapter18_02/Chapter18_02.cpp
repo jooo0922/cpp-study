20493ba28674f78efaab0e37a2e3115e00dba169
jooo0922 <jooo0922@gmail.com>
2024-02-12 18:04:36 +0900

    feat: 16진수 출력 설정

--- TBCppStudyChapter18/Chapter18_02/Chapter18_02.cpp
+++ TBCppStudyChapter18/Chapter18_02/Chapter18_02.cpp
@@ -10,13 +10,24 @@ int main()
 
     /* 출력 시 플래그 옵션 설정 */
 
-    /* cout.setf() 로 양수(+) 출력 */
+    /* cout.setf(std::ios::showpos) 로 양수(+) 출력 */
     cout.setf(std::ios::showpos);
     cout << 108 << endl;
 
     /* cout.unset() 으로 이전에 설정한 플래그 옵션 초기화 */
     cout.unsetf(std::ios::showpos);
     cout << 109 << endl;
+
+    /* cout.setf(std::ios::hex) 로 16진수 출력 */
+    // 10진수 모드를 먼저 초기화해야 됨.
+    /*cout.unsetf(std::ios::dec); 
+    cout.setf(std::ios::hex);*/
+    // 또는 아래와 같이 std::ios::basefield 로 모든 설정을 한꺼번에 초기화한 상태에서 16진수 출력 설정함.
+    //cout.setf(std::ios::hex, std::ios::basefield);
+    // 또는 아래와 같이 cout.setf() 를 사용하지 않고서, std::hex 만으로도 16진수 출력 설정 가능함. 
+    cout << std::hex; 
+    cout << 108 << endl;
+
 
     return 0;
 }
