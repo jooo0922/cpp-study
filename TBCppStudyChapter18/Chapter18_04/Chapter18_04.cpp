a61c2110464b67c80084f89af5d34800b7f2f46d
jooo0922 <jooo0922@gmail.com>
2024-02-12 19:45:48 +0900

    feat: 입력값이 원하는 문자인지 검사하는 함수 구현

--- TBCppStudyChapter18/Chapter18_04/Chapter18_04.cpp
+++ TBCppStudyChapter18/Chapter18_04/Chapter18_04.cpp
@@ -5,6 +5,30 @@
 
 using namespace std;
 
+// 내가 원하는 문자인지 검증하는 함수
+void printCharacterClassfication(const int& i)
+{
+    cout << boolalpha;
+
+    // 아래 검증 함수들은 <cctype> 에 있는 함수들을 가져온 것!
+    // 아래 함수들은 반환값이 int 타입이므로, bool() 로 캐스팅함.
+
+    // 알파벳(alphanumeric)인지 검사
+    cout << "isalnum " << bool(std::isalnum(i)) << endl;
+
+    // 공백 문자인지 검사
+    cout << "isblack " << bool(std::isblank(i)) << endl;
+    
+    // 십진수인지 검사
+    cout << "isdigit " << bool(std::isdigit(i)) << endl;
+    
+    // 소문자인지 검사
+    cout << "islower " << bool(std::islower(i)) << endl;
+    
+    // 대문자인지 검사
+    cout << "isupper " << bool(std::isupper(i)) << endl;
+}
+
 // 스트림 상태 출력 함수 
 // (참고로, std::ios 는 대부분의 stream 클래스들이 상속받는 상위 부모 클래스)
 void printState(const std::ios& stream)
@@ -20,7 +44,7 @@ int main()
 {
     while (true)
     {
-        int i;
+        char i;
 
         // 만약 입력 스트림에 int 타입이 아닌 문자열이 입력된다면 어떻게 유효성을 검증할 수 있을까?
         cin >> i;
@@ -45,6 +69,10 @@ int main()
 
         //// failbit 으로 비트마스킹하여 현재 입력 스트림의 fail 상태를 추출함
         //cout << !bool((cin.rdstate() & std::istream::failbit) != 0) << endl;
+
+
+        // 입력받은 값이 원하는 문자인지 검사
+        printCharacterClassfication(i);
 
 
         // 다음 반복문에서 새로운 입력값을 받기 위해, istream 버퍼를 깨끗이 비우는 작업
