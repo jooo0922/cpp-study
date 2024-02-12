0dbff9c96e996bd78b20683528e89eba84114fa4
jooo0922 <jooo0922@gmail.com>
2024-02-12 17:58:29 +0900

    feat: cout.setf() 로 양수 출력

--- TBCppStudyChapter18/Chapter18_02/Chapter18_02.cpp
+++ TBCppStudyChapter18/Chapter18_02/Chapter18_02.cpp
@@ -5,5 +5,15 @@ using namespace std;
 
 int main()
 {
+    /* ostream 출력 관련 설정 정리 */
+
+
+    /* 출력 시 플래그 옵션 설정 */
+
+    /* cout.setf() 로 양수(+) 출력 */
+    cout.setf(std::ios::showpos);
+    
+    cout << 108 << endl;
+    
     return 0;
 }
