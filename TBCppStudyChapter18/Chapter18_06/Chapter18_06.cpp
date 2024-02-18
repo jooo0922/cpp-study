39de23b03ea0c7bd2c74d36d163ab9d062cc841c
jooo0922 <jooo0922@gmail.com>
2024-02-18 14:13:48 +0900

    feat: 파일 출력 스트림으로 새로운 파일 생성

--- TBCppStudyChapter18/Chapter18_06/Chapter18_06.cpp
+++ TBCppStudyChapter18/Chapter18_06/Chapter18_06.cpp
@@ -8,5 +8,79 @@ using namespace std;
 
 int main()
 {
+    // writing
+    if (false)
+    {
+        // 파일 출력 스트림 std::ofstream 생성
+        /*
+            std::ofstream(std::string) 생성자는
+            
+            생성자 매개변수로 전달한 파일명이 존재하면, 
+            해당 파일을 열어주고,
+
+            존재하지 않으면, 
+            해당 이름으로 파일을 직접 만든 뒤,
+            파일을 열어주는 역할을 함.
+        */
+        ofstream ofs("my_first_file.dat");
+
+        // 파일을 열 때에는, 아래와 같이 std::ofstream.open() 함수로 직접 파일 열기 명령을 호출할 수도 있음!
+        //ofs.open("my_first_file.dat");
+
+        /*
+            파일 열기에 실패했다면,
+            아래와 같이 경고를 띄울 수 있도록
+            예외처리를 해놔야 안전하게 실행할 수 있음.
+
+            특히 파일 입출력은
+            이러한 예외처리를 신중하게 처리해놔야 함.
+        */
+        if (!ofs)
+        {
+            cerr << "Couldn't open file" << endl;
+            exit(1);
+        }
+
+        /*
+            다른 스트림 객체들처럼 
+            << 출력 스트림 연산자를 이용해서
+
+            열어둔 파일에 원하는 값을
+            출력(write) 할 수 있음!
+
+            참고로, 출력 스트림 연산자로
+            열려있는 파일에 write 할 경우,
+            텍스트 모드로 저장이 됨.
+            즉, ASCII 포맷으로 저장됨.
+
+            -> 따라서, 메모장에서 해당 파일을
+            열어서 write 된 값을 확인해볼 수 있음!
+        */
+        ofs << "Line 1" << endl;
+        ofs << "Line 2" << endl;
+
+        /*
+            파일 닫기 명령도 std::ofstream.close() 로 
+            직접 실행할 수 있지만,
+
+            대부분의 파일 입출력 작업은
+            특정 block {} 내에서 수행하는 경우가 많으므로,
+            
+            해당 block 이 끝나면
+            std::ofstream 도 메모리가 해제됨.
+
+            이때, 소멸자 함수에서
+            파일 닫기를 자동으로 실행하므로,
+            굳이 파일 닫기 함수인 .close() 를 직접 실행할 필요는 없음!
+        */
+        //ofs.close();
+    }
+
+    // reading
+    if (true)
+    {
+        ifstream ifs("my_first_file.dat", ios::binary);
+    }
+
     return 0;
 }
