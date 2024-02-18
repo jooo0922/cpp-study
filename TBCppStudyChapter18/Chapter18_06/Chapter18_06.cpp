1fcafa97f79c93bcc6dc97f535e1b289e7537d63
jooo0922 <jooo0922@gmail.com>
2024-02-18 13:58:37 +0900

    chore: Chapter18_06 프로젝트 생성

---
+++ TBCppStudyChapter18/Chapter18_06/Chapter18_06.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream> // file stream 사용을 위해 포함
+#include <string>
+#include <cstdlib> // exit() 사용을 위해 포함
+#include <sstream> // std::stringstream 사용을 위해 포함
+
+using namespace std;
+
+int main()
+{
+    return 0;
+}
