41a82ef5798e01e146baa02d4eecb170140be2af
jooo0922 <jooo0922@gmail.com>
2024-02-18 14:52:55 +0900

    chore: Chapter18_07 프로젝트 생성

---
+++ TBCppStudyChapter18/Chapter18_07/Chapter18_07.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream> // 파일 스트림 사용을 위해 포함
+#include <string>
+#include <cstdlib> // exit() 사용을 위해 포함
+#include <sstream> // 문자열 스트림 사용을 위해 포함
+
+using namespace std;
+
+int main()
+{
+    return 0;
+}
