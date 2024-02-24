26788c48483d38a8fdf48fedbbcf243a7ca0ff9d
jooo0922 <jooo0922@gmail.com>
2024-02-25 01:36:01 +0900

    feat: 멀티쓰레딩 예제 구현

--- TBCppStudyChapter19/Chapter19_05/Chapter19_05.cpp
+++ TBCppStudyChapter19/Chapter19_05/Chapter19_05.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <future> // async, future, promise 사용을 위해 포함
+#include <thread>
+
+using namespace std;
 
 int main()
 {
+    // multi-threading (이전 강의에서 살펴봤던 멀티쓰레딩 예제)
+    {
+        int result;
+        std::thread t([&] { result = 1 + 2; });
+        t.join();
+        cout << result << endl;
+    }
+
     return 0;
 }
