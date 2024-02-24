8562acb578ebcab220ac0b76cad36621aa6bed05
jooo0922 <jooo0922@gmail.com>
2024-02-24 18:01:27 +0900

    feat: std::vector 를 사용한 멀티쓰레딩 구현

--- TBCppStudyChapter19/Chapter19_03/Chapter19_03.cpp
+++ TBCppStudyChapter19/Chapter19_03/Chapter19_03.cpp
@@ -9,8 +9,23 @@ using namespace std;
 
 int main()
 {
-    // 현재 컴퓨터에서 사용할 수 있는 논리 프로세서의 갯수를 unsigned int 타입으로 반환 
-    // ㄷ-> 이 갯수를 가지고서 std::thread 에서 사용 가능!
+    /*
+        std::thread::hardware_concurrency()
+
+
+        현재 컴퓨터에서 사용할 수 있는 논리 프로세서의 갯수를 
+        unsigned int 타입으로 반환
+
+        thread 는 CPU 의 각 코어(프로세서)에 작업을 위임해서
+        동시에 처리할 수 있도록 하는 단위이므로,
+
+        최대 생성할 수 있는 thread 의 갯수는
+        CPU 가 제공하는 논리 프로세서 갯수(코어와 같은 경우도 있고, 더 많은 경우도 있음.)
+        를 넘을 수 없음.
+
+        따라서, std::thread 생성 및 사용 시, 
+        이 논리 프로세서 갯수만큼의 thread 만 생성이 가능하겠지!
+    */
     const int num_pro = std::thread::hardware_concurrency();
 
     // 현재 main() 함수가 실행되고 있는 thread 의 id 출력
@@ -21,31 +36,27 @@ int main()
 
     // std::thread 를 생성한 뒤, 작업시킬 함수를 함수 포인터, 람다 함수 등으로 매개변수에 전달
     // https://github.com/jooo0922/cpp-study/blob/main/TBCppStudyChapter19/Chapter19_01/Chapter19_01.cpp 참고
-    std::thread t1 = std::thread([]() {
-        // 새롭게 생성해서 전달받은 작업을 수행하는 thread 의 id 출력 (Main Thread 와 당연히 다름!)
-        cout << std::this_thread::get_id() << endl;
-        while (true) {}}
-    );
+    //std::thread t1 = std::thread([]() {
+    //    // 새롭게 생성해서 전달받은 작업을 수행하는 thread 의 id 출력 (Main Thread 와 당연히 다름!)
+    //    cout << std::this_thread::get_id() << endl;
+    //    while (true) {}}
+    //);
 
-    // 여러 개의 std::thread 를 실행해서 cpu 사용량 측정해보기
-    std::thread t2 = std::thread([]() {
-        // 새롭게 생성해서 전달받은 작업을 수행하는 thread 의 id 출력 (Main Thread 와 당연히 다름!)
-        cout << std::this_thread::get_id() << endl;
-        while (true) {}}
-    );
+    
+    /* std::vector 동적 배열로 여러 개의 thread 생성 및 사용 */
+    
+    vector<std::thread> my_threads;
+    my_threads.resize(num_pro);
 
-    std::thread t3 = std::thread([]() {
-        // 새롭게 생성해서 전달받은 작업을 수행하는 thread 의 id 출력 (Main Thread 와 당연히 다름!)
-        cout << std::this_thread::get_id() << endl;
-        while (true) {}}
-    );
-
-    std::thread t4 = std::thread([]() {
-        // 새롭게 생성해서 전달받은 작업을 수행하는 thread 의 id 출력 (Main Thread 와 당연히 다름!)
-        cout << std::this_thread::get_id() << endl;
-        while (true) {}}
-    );
-
+    // for-each 문으로 순회하며 std::thread 생성 및 사용
+    for (auto& e : my_threads)
+    {
+        e = std::thread([]() {
+            // 새롭게 생성해서 전달받은 작업을 수행하는 thread 의 id 출력 (Main Thread 와 당연히 다름!)
+            cout << std::this_thread::get_id() << endl;
+            while (true) {}}
+        );
+    }
 
     /*
         main() 함수가 돌고 있는 Main Thread 에서
@@ -63,13 +74,18 @@ int main()
         작업 완료 후, 다시 Main Thread 에
         결과값을 갖고 복귀해서 합류하도록 하는 것이
         std::thread.join() 함수라고 보면 됨!
+
+        달리 말하면, join() 함수 호출부는
+        각 thread 들이 끝나기를 기다리고 있는 것으로도
+        볼 수 있음!
     */
-    t1.join();
+    //t1.join();
 
-    // 항상 생성된 모든 thread 들은 작업을 마치면 Main Thread 로 join 시켜줘야 함!
-    t2.join();
-    t3.join();
-    t4.join();
-
+    // for-each 문으로 순회하며 작업이 완료된 thread 들을 Main Thread 에 join
+    for (auto& e : my_threads)
+    {
+        e.join();
+    }
+    
     return 0;
 }
