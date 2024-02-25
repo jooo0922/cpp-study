db143901d23982c64112f56b189b06f3b2ebd85e
jooo0922 <jooo0922@gmail.com>
2024-02-25 18:04:03 +0900

    feat: std::inner_product() 를 사용한 내적 계산 예제 구현

--- TBCppStudyChapter19/Chapter19_06/Chapter19_06.cpp
+++ TBCppStudyChapter19/Chapter19_06/Chapter19_06.cpp
@@ -51,5 +51,40 @@ int main()
         v1.push_back(uniformDist(engine));
     }
 
+    // 멀티쓰레딩으로 구현할 벡터 내적 연산과의 결과 비교를 위해 std::inner_product 먼저 실행
+    cout << "std::inner_product" << endl;
+    {
+        const auto sta = chrono::steady_clock::now(); // 내적 연산이 시작된 시점 저장
+
+        /*
+            std::inner_product 는
+            두 std::vector 의 반복자를 세 번째 매개변수까지 입력함.
+
+            두 번째 vector 의 v1.end() 를 입력하지 않은 이유는,
+            어차피 내적 연산할 v0 과 v1 의 컴포넌트 길이가 같다고 가정하기 때문!
+
+            또한, 100,000,000 개의 컴포넌트를 갖는 벡터의 내적이므로,
+            결과값이 아주 큰 정수값으로 나올 것이기 때문에,
+
+            0ull, 즉 '부호가 없는 long long 정수 타입의 리터럴'을 
+            네 번째 매개변수에 전달함으로써, 벡터 내적의 계산 결과 타입을 설정함.
+
+            0ull 에서 
+            '0'은 정수 리터럴이고,
+            'ull' 은 'unsigned long long'을 나타내는 접미사
+        */
+        const auto sum = std::inner_product(v0.begin(), v0.end(), v1.begin(), 0ull);
+
+        // 내적 연산 종료 시점에서 시작 시점을 빼서 경과시간 기록
+        const chrono::duration<double> dur = chrono::steady_clock::now() - sta;
+
+        // 경과시간 출력
+        cout << dur.count() << endl;
+
+        // 내적 연산 결과 출력
+        cout << sum << endl;
+        cout << endl;
+    }
+
     return 0;
 }
