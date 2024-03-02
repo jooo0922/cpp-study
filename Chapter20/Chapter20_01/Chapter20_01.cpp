617c1994c13f7678f7f91b2e191999606d950833
jooo0922 <jooo0922@gmail.com>
2024-03-03 00:58:16 +0900

    chore: Chapter20_01 프로젝트 생성

---
+++ Chapter20/Chapter20_01/Chapter20_01.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <random>
+#include <numeric> // std::inner_product 사용을 위해 포함
+
+using namespace std;
+
+int main()
+{
+    return 0;
+}
