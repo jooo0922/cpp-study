87b05b94c08b6bd09479dbcfcc8042c18f3425b4
jooo0922 <jooo0922@gmail.com>
2024-03-03 01:01:14 +0900

    feat: 프로파일링 예제 코드 구현

--- Chapter20/Chapter20_01/Chapter20_01.cpp
+++ Chapter20/Chapter20_01/Chapter20_01.cpp
@@ -6,7 +6,47 @@
 
 using namespace std;
 
+// 백만 번 더하기
+void func1()
+{
+    long long sum = 0;
+	for (unsigned i = 0; i < 1000'000; i++)
+	{
+		sum += 1;
+	}
+
+	cout << sum << endl;
+}
+
+// 10억 번 더하기 -> 연산량이 1000 배 더 많음
+void func2()
+{
+	long long sum = 0;
+	for (unsigned i = 0; i < 1000'000'000; i++)
+	{
+		sum += 1;
+	}
+
+	cout << sum << endl;
+}
+
+void run()
+{
+	func1();
+	func2();
+}
+
 int main()
 {
+	// 이 호출부에서 cpu 사용량 profiling
+	run();
+
+	// 이 부분에서 힙 메모리 profiling
+	int* d = new int[10000];
+	float* f = new float[100];
+
+	delete[] d;
+	delete[] f;
+
     return 0;
 }
