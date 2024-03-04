5a314ab8029ead8b4ffe43e493f9f46a80b6c116
jooo0922 <jooo0922@gmail.com>
2024-03-04 22:44:25 +0900

    chore: Chapter20_05_Server 프로젝트 생성

---
+++ Chapter20/Chapter20_05_Server/Chapter20_05_Server.cpp
@@ -0,0 +1,11 @@
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <boost/asio.hpp> // asio 를 사용한 TCP/IP 네트워킹 구현을 위해 포함
+
+/* asio 를 사용하여 서버-클라이언트 모델을 구현해보자 */
+
+int main()
+{
+    return 0;
+}
